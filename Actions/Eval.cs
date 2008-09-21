bac9f06d428498694124c30530125d9ab332506d
kino.coursey <kino.coursey@d53da353-1555-0410-a7d6-5d05f50501c2>
2008-09-21 05:25:10 +0000

    Fixes and expands basic events Updates cogbot.lisp to handle new events Adds "eval <lisp expression>" as an command action Adds runprebuild2005.bat and runprebuild2008.bat for VS2005 and VS2008.

---
+++ Actions/Eval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using OpenMetaverse; //using libsecondlife;
+
+namespace cogbot.Actions
+{
+    class Eval : Action
+    {
+       public Eval(TextForm parent)
+            : base(parent)
+        {
+            helpString = " eval: Enqueue a lisp task. Eval <lisp expression>";
+        }
+       public override void acceptInput(string verb, Parser args)
+       {
+           //base.acceptInput(verb, args);
+           parent.enqueueLispTask(args.str);
+       }
+    }
+}
