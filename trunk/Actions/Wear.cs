7da3ec777706b2a6b072b233516ef378648477f8
kino.coursey <kino.coursey@d53da353-1555-0410-a7d6-5d05f50501c2>
2008-11-15 23:29:53 +0000

    Remove *.bak and adds Wear.cs

---
+++ trunk/Actions/Wear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse; //using libsecondlife;
+
+namespace cogbot.Actions
+{
+    class Wear : Action
+    {
+        public Wear(TextForm parent)
+            : base(parent)
+        {
+            helpString = "Usage: wear [outfit name] ";
+            usageString = helpString + "\r\n you can type  'wear /My Outfit/Dance Party";
+        }
+
+        public override void acceptInput(string verb, Parser args)
+        {
+            base.acceptInput(verb, args);
+            string target = String.Empty;
+            bool bake = true;
+            try
+            {
+                parent.output("wear args =(" + args.str + ").");
+                client.Appearance.WearOutfit(args.str.Split('/'), bake);
+            }
+            catch (InvalidOutfitException ex)
+            {
+                parent.output("Invalid outfit (" + ex.Message + ")" + args.str+".");
+            }
+
+
+
+        }
+    }
+}
