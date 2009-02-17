8e5408415262700fad343b1c29d8f053fc72f743
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-02-17 23:17:32 +0000

    Update to prebuild forgotten on last commit

--- trunk/runprebuild.sh
+++ trunk/runprebuild.sh
@@ -4,9 +4,12 @@ mv ./Properties/Resources.resx ./Properties/Resources.resx.noNant
 mv ./DotCYC/CycConnectionForm.resx ./DotCYC/CycConnectionForm.resx.noNant
 mv ./TextForm.resx ./TextForm.resx.noNant
 mv ./LoginForm.resx ./LoginForm.resx.noNant
+mkdir -p RESX
+mv TheOpenSims/Navigation/Debug/*.resx RESX/
 
 mono ./bin/Prebuild.exe  prebuild.xml /target nant
 
+mv RESX/* TheOpenSims/Navigation/Debug/
 
 mv ./Properties/Resources.resx.noNant ./Properties/Resources.resx
 mv ./DotCYC/CycConnectionForm.resx.noNant ./DotCYC/CycConnectionForm.resx
