8c876091c1e4947f0edb4a2a4f9945edd680eade
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-05-12 09:25:54 +0000

    This may work for creating scripts

--- Actions/Inventory/CreateScriptCommand.cs
+++ Actions/Inventory/CreateScriptCommand.cs
@@ -1,19 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using OpenMetaverse;
-using OpenMetaverse.Assets;
-using OpenMetaverse.Http;
-using System.Threading;
-using System.Windows.Forms;
-using Logger = OpenMetaverse.Logger;
 
 namespace cogbot.Actions
 {
-#if PORTIT
     public class CreateScriptCommand : Command
     {
-        Exception problem;
         public CreateScriptCommand(BotClient testClient)
         {
             Name = "createscript";
@@ -21,21 +13,10 @@ namespace cogbot.Actions
             Category = CommandCategory.Inventory;
         }
 
-        public void RequestCreateItemFromAsset(byte[] data, string name, string description, AssetType assetType,
-           InventoryType invType, UUID folderID,ManualResetEvent done)
+        void OnNoteUpdate(bool success, string status, UUID itemID, UUID assetID)
         {
-            problem = null;
-            Client.Inventory.RequestCreateItemFromAsset(data, name, description, assetType, invType, folderID, delegate(CapsClient client, long bytesReceived, long bytesSent,
-            long totalBytesToReceive, long totalBytesToSend)
-            {
-                Console.WriteLine("Progress " + name + " {0}/{1} {2}/{3}", bytesReceived, totalBytesToReceive, bytesSent, totalBytesToSend);
-            }, delegate(bool success, string status, UUID itemID, UUID assetID)
-            {
-                string str = "Create " + (success ? "successfull " : "failed") + " for " + name + "  as " + assetID + " status " + status;
-                Console.WriteLine(str);
-                if (!success) problem = new Exception(str);
-                done.Set();
-            });
+            if (success)
+                WriteLine("Script successfully uploaded, ItemID {0} AssetID {1}", itemID, assetID);
         }
 
         public override string Execute(string[] args, UUID fromAgentID)
@@ -55,66 +36,51 @@ namespace cogbot.Actions
             System.IO.StreamReader reader = new StreamReader(file);
             string body = reader.ReadToEnd();
 
+            // FIXME: Upload the script asset first. When that completes, call RequestCreateItem
             try
             {
-                MakeScript(file, body);
+                string desc = String.Format("{0} created by OpenMetaverse BotClient {1}", file, DateTime.Now);
+                // create the asset
+
+                Client.Inventory.RequestCreateItem(Client.Inventory.FindFolderForType(AssetType.LSLText),
+                    file, desc, AssetType.LSLText, UUID.Random(), InventoryType.LSL, PermissionMask.All,
+                    delegate(bool success, InventoryItem item)
+                    {
+                        if (success) // upload the asset
+                            Client.Inventory.RequestUpdateScriptAgentInventory(CreateScriptAsset(body), item.UUID, new InventoryManager.ScriptUpdatedCallback(OnNoteUpdate));
+                    }
+                );
                 return "Done";
-            }
-            catch (System.Exception e)
-            {
-                return "Error creating script. " + e;
-            }
-        }
 
-        public void MakeScript(string file, string body)
-        {
-            try
-            {
-                if (Client.Network.CurrentSim==null)
-                Client.Network.CurrentSim = Client.Network.Simulators[0];
-                Client.Settings.LOG_ALL_CAPS_ERRORS = true;
-                ManualResetEvent done = new ManualResetEvent(false);
-                new Thread(new ThreadStart(delegate()
-                {
-                    MakeScript1(file, body, done);
-                })).Start();
-
-                bool complete = true;
-                while (!complete) {
-                    complete = done.WaitOne(50,false);
-                    Application.DoEvents();
-                    Thread.Sleep(500);
-                }
             }
             catch (System.Exception e)
             {
                 Logger.Log(e.ToString(), Helpers.LogLevel.Error, Client);
-                throw e;
+                return "Error creating script.";
             }
-            if (problem != null) throw problem;
         }
-
-        private void MakeScript1(string file, string body, ManualResetEvent done)
-        {
-            string desc = String.Format("{0} created by Cogbot.exe {1}", file, DateTime.Now);
-            // create the asset
-            byte[] data = CreateScriptAsset(body);
-            RequestCreateItemFromAsset(data, file, desc,
-                AssetType.LSLText, InventoryType.LSL,
-                Client.Inventory.FindFolderForType(AssetType.LSLText), done);
-        }
-        
         /// <summary>
         /// </summary>
         /// <param name="body"></param>
         public static byte[] CreateScriptAsset(string body)
         {
-           AssetScriptText ast = new AssetScriptText(body);
-           //UUID assetID = UUID.Random();
-           ast.Encode();
-          // ast = new AssetScriptText(assetID, ast.AssetData);
-           return  ast.AssetData;
+            // Format the string body into Linden text
+            string lindenText = "";/* "Linden text version 1\n";
+            lindenText += "{\n";
+            lindenText += "LLEmbeddedItems version 1\n";
+            lindenText += "{\n";
+            lindenText += "count 0\n";
+            lindenText += "}\n";
+            lindenText += "Text length " + body.Length + "\n";*/
+            lindenText += body;
+            //lindenText += "}\n";
+
+            // Assume this is a string, add 1 for the null terminator
+            byte[] stringBytes = System.Text.Encoding.UTF8.GetBytes(lindenText);
+            byte[] assetData = new byte[stringBytes.Length]; //+ 1];
+            Array.Copy(stringBytes, 0, assetData, 0, stringBytes.Length);
+
+            return assetData;
         }
     }
-#endif
 }
