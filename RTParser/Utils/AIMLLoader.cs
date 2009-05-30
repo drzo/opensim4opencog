f23ebaab203b563bcde2b77de8bd47c66dcacfe5
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-05-30 14:53:00 +0000

    Allowing multiple <pattern>s per template

--- RTParser/Utils/AIMLLoader.cs
+++ RTParser/Utils/AIMLLoader.cs
@@ -166,38 +166,41 @@ namespace RTParser.Utils
         private void processCategory(XmlNode node, string topicName, string filename)
         {
             // reference and check the required nodes
-            XmlNode pattern = this.FindNode("pattern", node);
-            XmlNode template = this.FindNode("template", node);
-            XmlNode guard = this.FindNode("guard", node);
-
-            if (object.Equals(null, pattern))
+            List<XmlNode> patterns = this.FindNodes("pattern", node);
+            foreach (var pattern in patterns)
             {
-                throw new XmlException("Missing pattern tag in a node found in " + filename);
-            }
-            if (object.Equals(null, template))
-            {
-                throw new XmlException("Missing template tag in the node with pattern: " + pattern.InnerText + " found in " + filename);
-            }
+                XmlNode template = this.FindNode("template", node);
+                XmlNode guard = this.FindNode("guard", node);
 
-            string categoryPath = this.generatePath(node, topicName, false);
-
-            // o.k., add the processed AIML to the GraphMaster structure
-            if (categoryPath.Length > 0)
-            {
-                try
+                if (object.Equals(null, pattern))
                 {
-                    this.RProcessor.Graphmaster.addCategory(categoryPath, template.OuterXml, guard, filename);
-                    // keep count of the number of categories that have been processed
-                    this.RProcessor.Size++;
+                    throw new XmlException("Missing pattern tag in a node found in " + filename);
                 }
-                catch
+                if (object.Equals(null, template))
                 {
-                    this.RProcessor.writeToLog("ERROR! Failed to load a new category into the graphmaster where the path = " + categoryPath + " and template = " + template.OuterXml + " produced by a category in the file: " + filename);
+                    throw new XmlException("Missing template tag in the node with pattern: " + pattern.InnerText + " found in " + filename);
                 }
-            }
-            else
-            {
-                this.RProcessor.writeToLog("WARNING! Attempted to load a new category with an empty pattern where the path = " + categoryPath + " and template = " + template.OuterXml + " produced by a category in the file: " + filename);
+
+                string categoryPath = this.generatePath(pattern, node, topicName, false);
+
+                // o.k., add the processed AIML to the GraphMaster structure
+                if (categoryPath.Length > 0)
+                {
+                    try
+                    {
+                        this.RProcessor.Graphmaster.addCategory(categoryPath, template.OuterXml, guard, filename);
+                        // keep count of the number of categories that have been processed
+                        this.RProcessor.Size++;
+                    }
+                    catch
+                    {
+                        this.RProcessor.writeToLog("ERROR! Failed to load a new category into the graphmaster where the path = " + categoryPath + " and template = " + template.OuterXml + " produced by a category in the file: " + filename);
+                    }
+                }
+                else
+                {
+                    this.RProcessor.writeToLog("WARNING! Attempted to load a new category with an empty pattern where the path = " + categoryPath + " and template = " + template.OuterXml + " produced by a category in the file: " + filename);
+                }
             }
         }
 
@@ -209,10 +212,9 @@ namespace RTParser.Utils
         /// <param name="isUserInput">marks the path to be created as originating from user input - so
         /// normalize out the * and _ wildcards used by AIML</param>
         /// <returns>The appropriately processed path</returns>
-        public string generatePath(XmlNode node, string topicName, bool isUserInput)
+        private string generatePath(XmlNode pattern, XmlNode node, string topicName, bool isUserInput)
         {
             // get the nodes that we need
-            XmlNode pattern = this.FindNode("pattern", node);
             XmlNode that = this.FindNode("that", node);
 
             string patternText;
@@ -249,6 +251,18 @@ namespace RTParser.Utils
                 }
             }
             return null;
+        }
+        private List<XmlNode> FindNodes(string name, XmlNode node)
+        {
+            List<XmlNode> nodes = new List<XmlNode>();
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                if (child.Name == name)
+                {
+                    nodes.Add(child);
+                }
+            }
+            return nodes;
         }
 
         /// <summary>
