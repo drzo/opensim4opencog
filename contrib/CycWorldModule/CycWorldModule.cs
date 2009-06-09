e5a81c53505402e0a48b98d88a66fb09c5a3bbbc
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-06-09 07:03:57 +0000

    Module now connects to Cyc and creates basic Mt support

--- contrib/CycWorldModule/CycWorldModule.cs
+++ contrib/CycWorldModule/CycWorldModule.cs
@@ -4,13 +4,15 @@ using System.Text;
 using System.Threading;
 using cogbot;
 using cogbot.Listeners;
-using cogbot.DotCYC;
+using CycWorldModule.DotCYC;
 
 namespace CycWorldModule
 {
     public class CycWorldModule : WorldObjectsModule
     {
+        private SimCyclifier cyclifier;
         private static CycWorldModule cycModule;
+        private CycConnectionForm cycConnectionForm;
         readonly static object oneInstanceLock = new object();
         public CycWorldModule(BotClient _parent)
             : base(_parent)
@@ -20,12 +22,24 @@ namespace CycWorldModule
                 {
                     cycModule = this;
                     _parent.ClientManager.AddTool("CycWorldModule","CycWorldModule", ShowCycForm);
+                    cyclifier = new SimCyclifier(this);
+
                 }
+        }
+
+        public CycConnectionForm CycConnectionForm
+        {
+            get
+            {
+                if (cycConnectionForm == null || cycConnectionForm.IsDisposed) 
+                    cycConnectionForm = new CycConnectionForm();
+                return cycConnectionForm;
+            }
         }
 
         private void ShowCycForm(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            CycConnectionForm.Reactivate();
         }
 
 
