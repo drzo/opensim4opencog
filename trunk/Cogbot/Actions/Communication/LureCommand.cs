6e162199e1b9225714e458b190c5d2837707cada
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-06-19 12:01:43 +0000

    Added Teleport Lure command usage: "lure some human"

---
+++ trunk/Cogbot/Actions/Communication/LureCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace cogbot.Actions.Communication
+{
+    class LureCommand    :Command
+    {
+        public LureCommand(BotClient testClient)
+        {
+            Name = "lure";
+            Description = "Send a lure to a user. Usage: lure FirstName LastName";
+            Category = CommandCategory.Communication;
+        }
+
+
+        public override string Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            string user = string.Join(" ", args);
+            UUID id = WorldSystem.GetUserID(user);
+            if (id==UUID.Zero) return "cannot find " + user;
+            Client.Self.SendTeleportLure(id);
+            return "teleport Lure sent to " + user;
+        }
+    }
+}
