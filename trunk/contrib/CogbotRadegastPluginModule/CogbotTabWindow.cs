09d83ad04308fb595af6e12bb1d3ca935578505e
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-08-17 00:36:56 +0000

    Update for upstream Radegast

--- trunk/contrib/CogbotRadegastPluginModule/CogbotTabWindow.cs
+++ trunk/contrib/CogbotRadegastPluginModule/CogbotTabWindow.cs
@@ -508,7 +508,7 @@ namespace CogbotRadegastPluginModule
 
         private void rtbChat_LinkClicked(object sender, LinkClickedEventArgs e)
         {
-            instance.MainForm.processLink(e.LinkText);
+            instance.MainForm.ProcessLink(e.LinkText);
         }
 
         private void tbtnProfile_Click(object sender, EventArgs e)
