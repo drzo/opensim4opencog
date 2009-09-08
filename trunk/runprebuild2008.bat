ec59949d66a26efe967898d13b45580c5a61ac3b
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-09-08 04:41:07 +0000

    Added some missing dependencies needed for prebuild

--- trunk/runprebuild2008.bat
+++ trunk/runprebuild2008.bat
@@ -1,4 +1,9 @@
 REM Targets available in Prebuild: autotools,makefile,monodev,nant,sharpdev,sharpdev2,vs2002,vs2003,vs2005,vs2008,xcode
 bin\Prebuild.exe  prebuild.xml /target vs2008
 copy lib\Radegast\assemblies\*.dll bin\
+copy lib\Radegast\*.dll bin\
+copy lib\Radegast\*.pdb bin\
+copy lib\Radegast\*.config bin\
+copy lib\Radegast\*.so bin\
+copy lib\Radegast\*.dylib bin\
 pause
