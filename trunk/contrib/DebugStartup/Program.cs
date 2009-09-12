2758643997a34cda1cb091bdc4243be7d73e2008
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-09-12 12:39:27 +0000

    Added ADebugStartup - When this is the startup project, All projects will get built and then incrementally while debugging

---
+++ trunk/contrib/DebugStartup/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Radegast;
+using System.Windows.Forms;
+
+namespace DebugStartup
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            RadegastInstance instance = RadegastInstance.GlobalInstance;
+            Application.Run(instance.MainForm);
+            instance = null;
+        }
+    }
+}
