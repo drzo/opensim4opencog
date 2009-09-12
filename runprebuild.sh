87cdfc4ce99470c72f06d3ef5967d5faf8fff9ec
logicmoo <logicmoo@gmail.com>
2009-09-12 12:35:21 +0000

    Fixed Prebuild on Linux

--- runprebuild.sh
+++ runprebuild.sh
@@ -2,5 +2,10 @@
 
 mono ./bin/Prebuild.exe  prebuild.xml /target nant
 cp lib/Radegast/assemblies/*.dll bin/
+cp lib/Radegast/*.dll bin/
+cp lib/Radegast/*.pdb bin/
+cp lib/Radegast/*.config bin/
+cp lib/Radegast/*.so bin/
+cp lib/Radegast/*.dylib bin/
 echo copied assembies
 
