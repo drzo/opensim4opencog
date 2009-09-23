2f99d8e1acef415d624469e43fe2fce8008f4dae
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-09-23 20:44:46 +0000

    Removed some debug noise for Context Menu Listener

--- trunk/contrib/CogbotRadegastPluginModule/CogbotContextMenuListener.cs
+++ trunk/contrib/CogbotRadegastPluginModule/CogbotContextMenuListener.cs
@@ -19,28 +19,33 @@ namespace CogbotRadegastPluginModule
         private void Test_OnContentMenuItemClicked(object sender, RadegastContextMenuStrip.ContextMenuEventArgs e)
         {
             if (e.MenuItem==null) return;
-            Console.WriteLine("I hope you meant to " + e.MenuItem.Text + "  " + e.Selection + "!");
+            WriteLine("I hope you meant to " + e.MenuItem.Text + "  " + e.Selection + "!");
             if (!e.MenuItem.Enabled)
             {
-                Console.WriteLine("If not do not worry it was not enabled ");
+                WriteLine("If not do not worry it was not enabled ");
             }
         }
-    
+
+        static void WriteLine(string s)
+        {
+           //Console.WriteLine(s);
+        }
+
         private void Test_OnContentMenuItemSelected(object sender, RadegastContextMenuStrip.ContextMenuEventArgs e)
         {
             lock (_cogbotContextMenu)
             {
                 if (e.MenuItem == null)
                 {
-                    Console.WriteLine("The last menu selection is not hightlighted by the mouse anymore so do not click");
+                    WriteLine("The last menu selection is not hightlighted by the mouse anymore so do not click");
                 }
                 else if (!e.MenuItem.Enabled)
                 {
-                    Console.WriteLine("You cannot " + e.MenuItem.Text + " at this time to " + e.Selection);
+                    WriteLine("You cannot " + e.MenuItem.Text + " at this time to " + e.Selection);
                 }
                 else
                 {
-                    Console.WriteLine("You can " + e.MenuItem.Text + " " + e.Selection + " if you press enter or click");
+                    WriteLine("You can " + e.MenuItem.Text + " " + e.Selection + " if you press enter or click");
                 }
             }
         }
@@ -49,7 +54,7 @@ namespace CogbotRadegastPluginModule
         {
             lock (_cogbotContextMenu)
             {
-                Console.WriteLine("You can no longer see the Menu: " + TheirInterest);
+                WriteLine("You can no longer see the Menu: " + TheirInterest);
                 TheirInterest = null;
             }
         }
@@ -59,13 +64,13 @@ namespace CogbotRadegastPluginModule
             lock (_cogbotContextMenu)
             {
                 TheirInterest = e.Menu;
-                Console.WriteLine("You are looking at Menu: " + TheirInterest);
-                Console.WriteLine("The Item you are going to do something to is: " + e.Selection);
+                WriteLine("You are looking at Menu: " + TheirInterest);
+                WriteLine("The Item you are going to do something to is: " + e.Selection);
                 foreach (var item in e.Menu.AllChoices())
                     if (item.Enabled)
-                        Console.WriteLine(" You can: " + item.Text);
+                        WriteLine(" You can: " + item.Text);
                     else
-                        Console.WriteLine(" cannot: " + item.Text);
+                        WriteLine(" cannot: " + item.Text);
             }
         }
     }
