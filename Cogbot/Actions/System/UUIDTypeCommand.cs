e200c41e83a9bcb34561a5d11afe0c76e858c20f
logicmoo <logicmoo@gmail.com>
2009-09-23 20:41:35 +0000

    Added "uuidtype" command to introspect what the type of a UUID stands for DownloadCommand can take the AssetType name in is second argument

---
+++ Cogbot/Actions/System/UUIDTypeCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenMetaverse;
+using cogbot.Listeners;
+
+namespace cogbot.Actions
+{
+    public class UUIDTypeCommand : Command
+    {
+        public UUIDTypeCommand(BotClient testClient)
+        {
+            Name = "UUID Type";
+            Description = "Resolve the type of Object the UUID represents.  Usage: uuidtype b06c0586-da9a-473d-aa94-ee3ab5606e4d";
+            Category = CommandCategory.TestClient;
+        }
+
+        public override string Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            if (args.Length < 1) return Usage;
+            var v = WorldObjects.uuidTypeObject;
+            UUID uuid;
+            string botcmd = String.Join(" ", args, 0, args.Length).Trim();
+            UUIDTryParse(botcmd, out uuid);
+
+            object obj = null;
+            lock (v)
+            {
+                v.TryGetValue(uuid, out obj);
+            }
+            if (obj != null)
+            {
+                WriteLine("Object is of Type " + obj.GetType());
+            }
+            else
+            {
+                WriteLine("Object not found for UUID " + uuid);
+            }
+            return "Done with UUID " + uuid + " obj= " + obj;
+        }
+    }
+}
