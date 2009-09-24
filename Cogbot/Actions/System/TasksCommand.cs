a4c29573a2ef175f0d649e8042b066b64feeff86
logicmoo <logicmoo@gmail.com>
2009-09-24 10:23:58 +0000

    Added new command: "tasks"
    [03:16] textform> tasks
    [03:16] Idle NewObjectQueue Todo=0 Complete=47250
    [03:16] Idle World EventQueue Todo=0 Complete=1328
    [03:16] Idle Simulator catchup Todo=0 Complete=1
    [03:16] Busy SimAssetStore Todo=386 Complete=79
    [03:16] Idle ParentGrabber Todo=13316 Complete=8
    [03:16] TaskQueueHandlers: 5, threads: 0

---
+++ Cogbot/Actions/System/TasksCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using cogbot.Utilities;
+using OpenMetaverse;
+
+namespace cogbot.Actions
+{
+
+    public class TasksCommand : Command
+    {
+        public TasksCommand(BotClient testClient)
+        {
+            Description = "Shows the list of task queue statuses.  Usage: tasks";
+            Category = CommandCategory.TestClient;
+        }
+
+        public override string Execute(string[] args, UUID fromAgentID1, OutputDelegate WriteLine)
+        {
+            int n = 0;
+            lock (Client.botCommandThreads) foreach (Thread t in Client.botCommandThreads)
+                {
+                    n++;
+                    //System.Threading.ThreadStateException: Thread is dead; state cannot be accessed.
+                    //  at System.Threading.Thread.IsBackgroundNative()
+                    WriteLine("{0}: {1} IsAlive={2}", n, t.Name, t.IsAlive);
+                }
+            int found = 0;
+            lock (TaskQueueHandler.TaskQueueHandlers)
+                foreach (var queueHandler in TaskQueueHandler.TaskQueueHandlers)
+                {
+                    found++;
+                    WriteLine(queueHandler.ToString());
+                }
+            return "TaskQueueHandlers: " + found + ", threads: " + n;
+        }
+    }
+}
