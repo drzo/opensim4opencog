c0679b795ea14264d54aef6436b1409d52685379
logicmoo <logicmoo@gmail.com>
2009-09-24 21:09:35 +0000

    Removed unneeded prebuild references around System.Speech

--- runprebuild2008.bat
+++ runprebuild2008.bat
@@ -1,6 +1,6 @@
 REM Targets available in Prebuild: autotools,makefile,monodev,nant,sharpdev,sharpdev2,vs2002,vs2003,vs2005,vs2008,xcode
 bin\Prebuild.exe  prebuild.xml /target vs2008
-copy lib\Radegast\assemblies\*.dll bin\
+copy lib\Radegast\assemblies\*.* bin\
 copy lib\Radegast\*.dll bin\
 copy lib\Radegast\*.pdb bin\
 copy lib\Radegast\*.config bin\
