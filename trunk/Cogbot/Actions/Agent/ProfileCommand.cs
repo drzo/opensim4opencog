bae1096ddc4360b35a16857f1b1228f453109ea3
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-10-17 11:08:47 +0000

    Added new ProfileCommand to display a users profile  /profile master to show the masters profile + BotPermsCommand to control who the bot ignores.. the start of a security level for the bot

---
+++ trunk/Cogbot/Actions/Agent/ProfileCommand.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using cogbot.TheOpenSims;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+
+namespace cogbot.Actions
+{
+    public class ProfileCommand : Command
+    {
+
+        public ProfileCommand(BotClient testClient)
+        {
+
+            Name = "Profile";
+            Description = "Shows the Avatars profile in a UI component. Usage: profile <avatar>";
+            Category = CommandCategory.Friends;
+            Parameters = new [] {  new NamedParam(typeof(Avatar), typeof(UUID)) };
+        }
+
+        public override CmdResult Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            if (args.Length < 1)
+                return ShowUsage();
+            int argsUsed;
+            foreach (var p in WorldSystem.GetPrimitives(args, out argsUsed))
+            {
+                if (p is SimAvatar)
+                {
+                    SimAvatar a = (SimAvatar)p;
+                    Success("Showing " + a);
+                    TheBotClient.Invoke(() =>
+                                        new Radegast.frmProfile(TheBotClient.TheRadegastInstance, a.GetName(), a.ID).Show());
+                }
+            }            
+            return SuccessOrFailure();
+        }
+    }
+}
---
+++ trunk/Cogbot/Actions/System/BotPermsCommand.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+using cogbot.TheOpenSims;
+
+namespace cogbot.Actions
+{
+    public class BotPermsCommand : Command, BotSystemCommand
+    {
+        public BotPermsCommand(BotClient testClient)
+        {
+            Name = "botperms";
+            Description = "Sets the bot use permissions. Usage: botperms name [Base] [Owner] [Group] [Ignore]";
+            Category = CommandCategory.TestClient;
+        }
+
+        public override CmdResult Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            if (args.Length < 1)
+                return ShowUsage();
+            int argsUsed;
+            List<SimObject> worldSystemGetPrimitives = WorldSystem.GetPrimitives(args, out argsUsed);
+            if (IsEmpty(worldSystemGetPrimitives))
+            {
+                return Failure("Cannot find objects from " + string.Join(" ", args));
+            }
+            BotPermissions who =
+                (BotPermissions)
+                (EnumParse(typeof (BotPermissions), args, argsUsed, out argsUsed) ?? BotPermissions.Base);
+
+            foreach (var p in worldSystemGetPrimitives)
+            {
+
+                BotPermissions perms = TheBotClient.GetSecurityLevel(p.ID);
+                if (argsUsed==0)
+                {
+                    Success("Perms for " + p + " was " + perms);
+                    continue;    
+                }
+                Success("Perms for " + p + " was " + perms + " now setting to " + who);
+                TheBotClient.SecurityLevels[p.ID] = who;
+            }
+            return SuccessOrFailure();
+        }
+    }
+}
