e1957c91d5e6503414048282652fd0af804821fe
logicmoo <logicmoo@gmail.com>
2009-10-23 03:47:24 +0000

    Each RadegasInterp can use its own botclient

--- contrib/CogbotRadegastPluginModule/CogbotRadegastInterpreter.cs
+++ contrib/CogbotRadegastPluginModule/CogbotRadegastInterpreter.cs
@@ -6,10 +6,26 @@ namespace CogbotRadegastPluginModule
     public class CogbotRadegastInterpreter: Radegast.Commands.ICommandInterpreter
     {
         private ClientManager clientManager;
+        private BotClient botClient;
+        public BotClient BotClient
+        {
+            get
+            {
+                return botClient;
+            }
+            set
+            {
+                if (botClient == value)
+                    return;
+                botClient = value;
+            }
+        }
+
 
         public CogbotRadegastInterpreter(ClientManager manager)
         {
             clientManager = manager;
+            botClient = manager.LastBotClient;
         }
         public RadegastInstance RadegastInstance;
         public bool IsValidCommand(string cmdline)
@@ -24,7 +40,20 @@ namespace CogbotRadegastPluginModule
             {
                 cmdline = cmdline.Substring(1);
             }
-            WriteLine(clientManager.LastBotClient.ExecuteCommand(cmdline, new OutputDelegate(WriteLine)).ToString());
+            OutputDelegate newOutputDelegate = new OutputDelegate(WriteLine);
+            cogbot.Actions.CmdResult result;
+            if (botClient == null)
+            {
+                result = clientManager.ExecuteCommand(cmdline,newOutputDelegate);
+            }
+            else
+            {
+                result = botClient.ExecuteCommand(cmdline,newOutputDelegate);
+            }
+
+            if (result != null)
+                WriteLine(result.ToString());
+            else WriteLine("No result returned: {0}", cmdline);
         }
 
         public void Help(string helpArgs, ConsoleWriteLine WriteLine)
