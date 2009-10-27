d82c971395c869dc182b09681aa3b4411b701538
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2009-10-27 09:06:06 +0000

    Added SearchConsole to CogbotRadegastPlugin

---
+++ trunk/contrib/CogbotRadegastPluginModule/Search/FindEvents.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using OpenMetaverse;
+//using SLNetworkComm;
+using Radegast;
+using METAboltInstance = Radegast.RadegastInstance;
+using SLNetCom = Radegast.Netcom.RadegastNetcom;
+
+namespace METAbolt
+{
+    public partial class FindEvents : UserControl
+    {
+        private METAboltInstance instance;
+        private SLNetCom netcom;
+        private GridClient client;
+        private float fX;
+        private float fY;
+        private float fZ;
+
+        private ListViewItemComparer lvcompare;
+
+        private UUID queryID;
+        private Dictionary<string, uint> findEventsResults;
+
+        public event EventHandler SelectedIndexChanged;
+
+        public FindEvents(METAboltInstance instance, UUID queryID)
+        {
+            InitializeComponent();
+
+            findEventsResults = new Dictionary<string, uint>();
+            this.queryID = queryID;
+
+            this.instance = instance;
+            netcom = this.instance.Netcom;
+            client = this.instance.Client;
+            AddClientEvents();
+        }
+
+        private void AddClientEvents()
+        {
+            client.Directory.DirEventsReply += Directory_OnEventsReply;
+            client.Directory.EventInfoReply += eventsconsole_OnEventInfo;
+            
+        }
+
+        //Separate thread
+        private void Directory_OnEventsReply(object sender, DirEventsReplyEventArgs e)
+        {
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                EventsReply(e.QueryID, e.MatchedEvents);
+            }));
+        }
+
+        // Separate thread
+        private void eventsconsole_OnEventInfo(object sender, EventInfoReplyEventArgs e)
+        {
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                EventInf(e.MatchedEvent);
+            }));
+        }
+
+        // UI thread
+        private void EventInf(DirectoryManager.EventInfo matchedEvent)
+        {
+            textBox7.Text = matchedEvent.Creator.ToString();
+            textBox2.Text = matchedEvent.Name.ToString();
+            textBox3.Text = DirectoryManager.EventCategories.All.ToString();   // matchedEvent.Category.ToString();
+
+            if (matchedEvent.Duration > 59)
+            {
+                uint dur = matchedEvent.Duration/60;
+                textBox5.Text = dur.ToString() + " hours"; 
+            }
+            else
+            {
+                textBox5.Text = matchedEvent.Duration.ToString() + " minutes";
+            }
+            textBox6.Text = matchedEvent.Date.ToString();
+
+            // Get region handle
+            //ulong regionhand =Helpers.UIntsToLong((uint)(matchedEvent.GlobalPos.X - (matchedEvent.GlobalPos.X % 256)), (uint)(matchedEvent.GlobalPos.Y - (matchedEvent.GlobalPos.Y % 256)));
+            
+            // Convert Global pos to local
+            float locX = (float)matchedEvent.GlobalPos.X; ;
+            float locY = (float)matchedEvent.GlobalPos.Y;
+            float locX1;
+            float locY1;
+            Helpers.GlobalPosToRegionHandle(locX, locY, out locX1, out locY1); 
+
+            fX = locX1;
+            fY = locY1;
+            fZ = (float)matchedEvent.GlobalPos.Z;
+
+            textBox8.Text = matchedEvent.SimName.ToString() + "/" + fX.ToString() + "/" + fY.ToString() + "/" + fZ.ToString();
+
+            if (matchedEvent.Cover == 0)
+            {
+                textBox9.Text = "none";
+            }
+            else
+            {
+                textBox9.Text = "L$ " + matchedEvent.Cover.ToString();
+            }
+
+            textBox1.Text = matchedEvent.Desc.ToString();
+        }
+
+        //UI thread
+        private void EventsReply(UUID queryID, List<DirectoryManager.EventsSearchData> matchedEvents)
+        {
+            if (queryID != this.queryID) return;
+
+            lvwFindEvents.BeginUpdate();
+
+            foreach (DirectoryManager.EventsSearchData  events in matchedEvents)
+            {
+                try
+                {
+                    if (!findEventsResults.ContainsValue(events.ID))
+                    {
+                        string fullName = events.Name;
+                        findEventsResults.Add(fullName, events.ID);
+
+                        ListViewItem item = lvwFindEvents.Items.Add(fullName);
+                        item.SubItems.Add(events.Date);   // + "-" + events.Time);
+                    }
+                }
+                catch (Exception exc)
+                {
+                    string exp = exc.Message; 
+                }
+            }
+ 
+            lvwFindEvents.Sort();
+            lvwFindEvents.EndUpdate();
+            pEvents.Visible = false; 
+        }
+
+        public void ClearResults()
+        {
+            findEventsResults.Clear();
+            lvwFindEvents.Items.Clear();
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void lvwFindEvents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OnSelectedIndexChanged(e);
+
+            button1.Enabled = true;
+            button2.Enabled = true;  
+        }
+
+        protected virtual void OnSelectedIndexChanged(EventArgs e)
+        {
+            if (SelectedIndexChanged != null) SelectedIndexChanged(this, e);
+        }
+
+        public Dictionary<string, uint> LLUUIDs
+        {
+            get { return findEventsResults; }
+        }
+
+        public UUID QueryID
+        {
+            get { return queryID; }
+            set { queryID = value; }
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                if (lvwFindEvents.SelectedItems == null) return -1;
+                if (lvwFindEvents.SelectedItems.Count == 0) return -1;
+
+                return lvwFindEvents.SelectedIndices[0];
+            }
+        }
+
+        public string SelectedName
+        {
+            get
+            {
+                if (lvwFindEvents.SelectedItems == null) return string.Empty;
+                if (lvwFindEvents.SelectedItems.Count == 0) return string.Empty;
+
+                return lvwFindEvents.SelectedItems[0].Text;
+            }
+        }
+
+        public string SelectedTime
+        {
+            get
+            {
+                if (lvwFindEvents.SelectedItems == null) return "";
+                if (lvwFindEvents.SelectedItems.Count == 0) return "";
+
+                string sTime = lvwFindEvents.SelectedItems[0].SubItems[0].Text;
+
+                return sTime;
+            }
+        }
+
+        public uint SelectedEventUUID
+        {
+            get
+            {
+                if (lvwFindEvents.SelectedItems == null) return 0;
+                if (lvwFindEvents.SelectedItems.Count == 0) return 0;
+
+                string name = lvwFindEvents.SelectedItems[0].Text;
+                return findEventsResults[name];
+            }
+        }
+
+        private void FindEvents_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"http://secondlife.com/events/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sLoc = textBox8.Text;
+
+            char[] deli = "/".ToCharArray();
+            string[] iDets = sLoc.Split(deli);
+
+            //netcom.Teleport(iDets[0],
+            //client.Self.Teleport(   
+
+            var frmTeleport = new frmTeleport(instance);
+            frmTeleport.txtRegion.Text = iDets[0].ToString();
+            frmTeleport.nudX.Value = (int)fX;
+            frmTeleport.nudY.Value = (int)fY;
+            frmTeleport.nudZ.Value = (int)fZ;
+            frmTeleport.ShowDialog();
+        }
+
+        private void lvwFindEvents_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            this.lvwFindEvents.ListViewItemSorter = new ListViewItemComparer(e.Column);
+            // Call the sort method to manually sort.
+            lvwFindEvents.Sort();
+        }    
+    }
+}
---
+++ trunk/contrib/CogbotRadegastPluginModule/Search/FindPlaces.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using OpenMetaverse;
+using Radegast;
+using METAboltInstance = Radegast.RadegastInstance;
+using SLNetCom = Radegast.Netcom.RadegastNetcom;
+//using SLNetworkComm;
+
+namespace METAbolt
+{
+    public partial class FindPlaces : UserControl
+    {
+        private METAboltInstance instance;
+        private SLNetCom netcom;
+        private GridClient client;
+        private float fX;
+        private float fY;
+        private float fZ;
+        private string sSIM;
+
+        //private ListViewItemComparer lvcompare;
+
+        private UUID queryID;
+        private Dictionary<string, DirectoryManager.PlacesSearchData> findPlacesResults;
+        private DirectoryManager.PlacesSearchData EmptyPlace;
+
+        public event EventHandler SelectedIndexChanged;
+
+        public FindPlaces(METAboltInstance instance, UUID queryID)
+        {
+            InitializeComponent();
+
+            findPlacesResults = new Dictionary<string, DirectoryManager.PlacesSearchData>();
+            this.queryID = queryID;
+
+            this.instance = instance;
+            netcom = this.instance.Netcom;
+            client = this.instance.Client;
+            AddClientEvents();
+        }
+
+        private void AddClientEvents()
+        {
+            client.Directory.PlacesReply += Directory_OnPlacesReply;
+        }
+
+        //Separate thread
+        private void Directory_OnPlacesReply(object sender, PlacesReplyEventArgs e)
+        {
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                PlacesReply(e.QueryID, e.MatchedPlaces);
+            }));
+        }
+
+        private void PlacesReply(UUID queryID, List<DirectoryManager.PlacesSearchData> matchedPlaces)
+        {
+            if (queryID != this.queryID) return;
+
+            lvwFindPlaces.BeginUpdate();
+
+            foreach (DirectoryManager.PlacesSearchData places in matchedPlaces)
+            {
+                int icnt = 0;
+                try
+                {
+                    if (!findPlacesResults.ContainsValue(places))
+                    {
+                        string fullName = places.Name;
+                        findPlacesResults.Add(fullName, places);
+                        
+                        ListViewItem item = lvwFindPlaces.Items.Add(fullName);
+                        item.SubItems.Add(places.Dwell.ToString());   // + "-" + events.Time);
+                    }
+
+                    icnt += 1;
+                }
+                catch (Exception exc)
+                {
+                    string exp = exc.Message;
+                }
+            }
+
+            lvwFindPlaces.Sort();
+            lvwFindPlaces.EndUpdate();
+            pPlaces.Visible = false; 
+        }
+
+        // UI thread
+        public void DisplayPlace(DirectoryManager.PlacesSearchData place)
+        {
+            if (place.Name == null)
+                return;
+
+            string sForSale = "";
+
+            if (place.Price > 0)
+            {
+                sForSale = "For Sale for L$" + place.Price.ToString();   
+            }
+
+            txtName.Text = place.Name;
+            txtDescription.Text = place.Desc;
+            txtInformation.Text = "Traffic: " + place.Dwell + " Area: " + place.ActualArea.ToString() + " sq. m. " + sForSale;
+
+
+            // Can't figure out how this is done so I am commenting
+            // this feature out. If you know how it's done
+            // go ahead and implement it
+
+            //string LandFlag = "";
+
+            //if ((place.Flags & Parcel.ParcelFlags.MaturePublish) == Parcel.ParcelFlags.MaturePublish)
+            //{
+            //    byte sflag = place.Flags;
+            //    LandFlag = " (Mature)";
+            //}
+
+            //txtLocation.Text += LandFlag;
+
+            // Convert Global pos to local
+            float locX = (float)place.GlobalX; ;
+            float locY = (float)place.GlobalY;
+            float locX1;
+            float locY1;
+            Helpers.GlobalPosToRegionHandle(locX, locY, out locX1, out locY1);
+
+            fX = locX1;
+            fY = locY1;
+            fZ = (float)place.GlobalZ;
+            sSIM = place.SimName;  
+
+            txtLocation.Text = place.SimName.ToString() + " " + fX.ToString() + ", " + fY.ToString() + ", " + fZ.ToString();
+        }
+
+        public void ClearResults()
+        {
+            findPlacesResults.Clear();
+            lvwFindPlaces.Items.Clear();
+            button1.Enabled = false;
+        }
+
+        private void lvwFindPlaces_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OnSelectedIndexChanged(e);
+
+            button1.Enabled = true;
+        }
+
+        protected virtual void OnSelectedIndexChanged(EventArgs e)
+        {
+            if (SelectedIndexChanged != null) SelectedIndexChanged(this, e);
+        }
+
+
+        public UUID QueryID
+        {
+            get { return queryID; }
+            set { queryID = value; }
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                if (lvwFindPlaces.SelectedItems == null) return -1;
+                if (lvwFindPlaces.SelectedItems.Count == 0) return -1;
+
+                return lvwFindPlaces.SelectedIndices[0];
+            }
+        }
+
+        public DirectoryManager.PlacesSearchData SelectedName
+        {
+            get
+            {
+                if (lvwFindPlaces.SelectedItems == null) return EmptyPlace;
+                if (lvwFindPlaces.SelectedItems.Count == 0) return EmptyPlace;
+
+                string name = lvwFindPlaces.SelectedItems[0].Text;
+                return findPlacesResults[name];
+            }
+        }
+
+        private void FindPlaces_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwFindPlaces_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            this.lvwFindPlaces.ListViewItemSorter = new ListViewItemComparer(e.Column);
+            // Call the sort method to manually sort.
+            lvwFindPlaces.Sort();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var frmTeleport = new frmTeleport(instance);
+            frmTeleport.txtRegion.Text = sSIM;
+            frmTeleport.nudX.Value = (int)fX;
+            frmTeleport.nudY.Value = (int)fY;
+            frmTeleport.nudZ.Value = (int)fZ;
+            frmTeleport.ShowDialog();
+        }
+    }
+}
