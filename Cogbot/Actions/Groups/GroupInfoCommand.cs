bfe9d5c2e55207d371ca22ff6de7f1e434bb6430
logicmoo <logicmoo@gmail.com>
2009-11-13 05:53:00 +0000

    Added GroupInfo command //groupinfo [name-or-uuid]

---
+++ Cogbot/Actions/Groups/GroupInfoCommand.cs
@@ -0,0 +1,45 @@
+using System;
+using OpenMetaverse;
+
+namespace cogbot.Actions
+{
+    /// <summary>
+    /// Shows group info Dialog using Radegast UI
+    /// </summary>
+    public class GroupInfoCommand : Command, GridMasterCommand
+    {
+        private string GroupName;
+        private UUID GroupUUID;
+
+        public GroupInfoCommand(BotClient testClient)
+        {
+            Name = "groupinfo";
+            Description = "Shows the group UI. Usage: groupinfo GroupName";
+            Category = CommandCategory.Groups;
+        }
+
+        public override CmdResult Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            if (args.Length < 1)
+                return ShowUsage();
+
+            GroupName = String.Empty;
+            for (int i = 0; i < args.Length; i++)
+                GroupName += args[i] + " ";
+            GroupName = GroupName.Trim();
+
+            GroupUUID = Client.GroupName2UUID(GroupName);
+            if (UUID.Zero != GroupUUID)
+            {
+                TheBotClient.Invoke(() =>
+                {
+                    Group group = new Group { ID = GroupUUID };
+                    TheBotClient.TheRadegastInstance.MainForm.ShowGroupProfile(group);
+                });
+
+                return Failure("Shown group " + GroupName + " " + GroupUUID);
+            }
+            return Failure("Cannot find group " + GroupName);
+        }
+    }
+}
