90f3ff5043f2f8ce9de8e1bd2cba57c7457216cb
logicmoo <logicmoo@gmail.com>
2009-11-23 01:08:19 +0000

    [11:21] <kinoc> motified Aiml set/get to use a "global_name" to set and get global background default variables [11:22] <kinoc> so if the bot hears an object say something it gives the object its own user [11:22] <kinoc> which is a problem if the user is playing with something or running a couple pose set [11:23] <kinoc> i.e. a pose set for themselves and the bot [11:23] <kinoc> I have it set the "topic" but that ended up only working for the object, not the user [11:24] <kinoc> when what I want is for the bot to act like it knows the context based on what the object says [11:24] <kinoc> like someone calling out a square dance [11:24] <kinoc> So ... [11:25] <kinoc> so before the AIML get returns a value [11:25] <kinoc> it checks if there is a globalPredicate value for user "globalPredicate" [11:25] <kinoc> and uses that if the local variable is either empty or the wild card [11:26] <kinoc> and you set in in parallel using something like [11:27] <kinoc>         <set name="topic" global_name="topic"> [11:27] <kinoc>           <get name="bedtopic"/> [11:27] <kinoc>         </set>

--- RTParser/AIMLTagHandlers/set.cs
+++ RTParser/AIMLTagHandlers/set.cs
@@ -47,16 +47,23 @@ namespace RTParser.AIMLTagHandlers
             if (this.templateNode.Name.ToLower() == "set")
             {
                 Unifiable name = GetAttribValue("name", null);
+                Unifiable gName = GetAttribValue("global_name", null);
+                // try to use a global blackboard predicate
+                bool newlyCreated;
+                RTParser.User gUser = this.user.bot.FindOrCreateUser("globalPreds", out newlyCreated);
+
                 if (!Unifiable.IsNull(name))
                 {
                     if (!templateNodeInnerText.IsEmpty)
                     {
+                        if (!Unifiable.IsNull(gName)) gUser.Predicates.addSetting(gName, templateNodeInnerText);
                         this.user.Predicates.addSetting(name, templateNodeInnerText);
                         return this.user.Predicates.grabSetting(name);
                     }
                     else
                     {
                         // remove the predicate
+                        if (!Unifiable.IsNull(gName)) gUser.Predicates.removeSetting(gName);
                         this.user.Predicates.removeSetting(name);
                         return Unifiable.Empty;
                     }
@@ -73,6 +80,7 @@ namespace RTParser.AIMLTagHandlers
                         this.user.Predicates.removeSetting(name);
                         return Unifiable.Empty;
                     }
+                    if (!Unifiable.IsNull(gName)) gUser.Predicates.addSetting(gName, templateNodeInnerText);
                     this.user.Predicates.addSetting(name, joined);
                     return this.user.Predicates.grabSetting(name);
                 }
