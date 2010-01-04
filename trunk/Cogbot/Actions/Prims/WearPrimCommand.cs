b05594c82f3fe4009151dcf1c31b3fc60de81fff
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-01-04 09:31:10 +0000

    TakeCommand and WearPrimCommand uses SimAvatar API now

--- trunk/Cogbot/Actions/Prims/WearPrimCommand.cs
+++ trunk/Cogbot/Actions/Prims/WearPrimCommand.cs
@@ -26,50 +26,18 @@ namespace cogbot.Actions
             {
                 return ShowUsage();
             }
-            List<UUID> Received = new List<UUID>();
-            ManualResetEvent ItemsRecieved = new ManualResetEvent(false);
 
             int argsUsed;
             List<SimObject> PS = WorldSystem.GetPrimitives(args, out argsUsed);
             if (IsEmpty(PS)) return Failure("Cannot find objects from " + string.Join(" ", args));
-            GridClient client = TheBotClient;
-
-            EventHandler<ItemReceivedEventArgs> onItemReceived = (sender, e) => {
-                                                                       var item = e.Item;
-                                                                       lock (Received) if (Received.Contains(item.AssetUUID))
-                                                                       {
-                                                                           ItemsRecieved.Set();
-                                                                           Received.Remove(item.AssetUUID);
-                                                                           List<InventoryItem> newList = new List<InventoryItem> { item };
-                                                                           Client.Appearance.AddToOutfit(newList);
-                                                                       }
-            };
-
-           ;
-            Client.Inventory.ItemReceived += onItemReceived;
 
             foreach (var currentPrim in PS)
             {
                 Success(Name + " on " + currentPrim);
-                lock (Received) Received.Add(currentPrim.ID);
-                client.Inventory.RequestDeRezToInventory(currentPrim.LocalID, DeRezDestination.AgentInventoryTake,
-                                                         client.Inventory.FindFolderForType(AssetType.Object), UUID.Zero);
-            }
-            try
-            {
-                //30 secs
-                if (!ItemsRecieved.WaitOne(30000))
-                {
-                    return Failure("Timeout on reciveing items");
-                }
-                return SuccessOrFailure();
+                if (!TheSimAvatar.AttachToSelf(currentPrim)) Failure("Cannot Attach " + currentPrim);
 
             }
-            finally
-            {
-                Client.Inventory.ItemReceived -= onItemReceived;
-            }
-
+            return SuccessOrFailure();
         }
     }
 }
