f6a992f6e52cebe32563d8ab5800be0b61c54e06
logicmoo <logicmoo@gmail.com>
2010-04-13 17:59:13 +0000

    WebUtils -> External  + 2 New Commands ShellExec and JarExec

---
+++ Cogbot/Actions/External/ShellExec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Net;
+//using OpenMetaverse; //using libsecondlife;
+
+namespace cogbot.Actions.External
+{
+    public class ShellExec : Command, SystemApplicationCommand
+    {
+        public static string DoShellExec(string cmd, string args)
+        {
+            Process proc = new Process();
+            proc.EnableRaisingEvents = false;
+            proc.StartInfo.FileName = cmd;
+            proc.StartInfo.Arguments = args;
+            proc.Start();
+            var output = proc.StandardOutput;
+            proc.WaitForExit();
+            return output.ReadToEnd();
+        }
+
+        public ShellExec(BotClient Client)
+            : base(Client)
+        {
+            Name = "ShellExec";
+            Description = "Do an shell exec. Usage: shellexec dir";
+        }
+        public override CmdResult acceptInput(string verb, Parser args, OutputDelegate WriteLine)
+        {
+            return Success(DoShellExec(args[0],Parser.Rejoin(args.tokens,1)));
+        }
+    }
+}
