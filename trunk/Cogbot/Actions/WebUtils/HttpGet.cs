4cd2282b3b3856b5835c7146b141d59ca17278bb
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-04-13 02:01:39 +0000

    Initial Checkin of HttpPost/Get commands from command interface .. no exception handling in the commands its done outside the commands

---
+++ trunk/Cogbot/Actions/WebUtils/HttpGet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+//using OpenMetaverse; //using libsecondlife;
+
+namespace cogbot.Actions.WebUtil
+{
+    public class HttpGet : Command, SystemApplicationCommand
+    {
+        public static string DoHttpGet(string url)
+        {
+            return Encoding.UTF8.GetString((new WebClient()).DownloadData(url));
+        }
+
+        public HttpGet(BotClient Client)
+            : base(Client)
+        {
+            Name = "HttpGet";
+            Description = "Do an http get. Usage: HttpGet http://localhost:5580/action?cmd=say&args=hello";
+        }
+        public override CmdResult acceptInput(string verb, Parser args, OutputDelegate WriteLine)
+        {
+            return Success(DoHttpGet(args.str));
+        }
+    }
+}
