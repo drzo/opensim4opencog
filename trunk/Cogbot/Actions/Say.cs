d6eefdc60a7389d18e98fcd02bf916e5512eae37
kino.coursey <kino.coursey@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-04-23 20:43:10 +0000

    Have to catch the <sapi> in the Say command. Probably can do it sooner, but it is both safe and effective here for now.

--- trunk/Cogbot/Actions/Say.cs
+++ trunk/Cogbot/Actions/Say.cs
@@ -18,12 +18,32 @@ namespace cogbot.Actions
         public override CmdResult acceptInput(string verb, Parser args, OutputDelegate WriteLine)
         {
           //  base.acceptInput(verb, args);
+           
+            string text = args.str;
 
             if (args.str.Length > 0)
             {
-                TheBotClient.Talk(args.str);
+                if (text.Contains("<sapi>"))
+                {
+                    // example fragment
+                    // <sapi> <silence msec="100" /> <bookmark mark="anim:hello.csv"/> Hi there </sapi>
+                    text = text.Replace("<sapi>", "");
+                    text = text.Replace("</sapi>", "");
+                    while (text.Contains("<"))
+                    {
+                        int p1 = text.IndexOf("<");
+                        int p2 = text.IndexOf(">", p1);
+                        if (p2 > p1)
+                        {
+                            string fragment = text.Substring(p1, (p2 + 1) - p1);
+                            text = text.Replace(fragment, " ");
+                        }
+                    }
+
+                }
+                TheBotClient.Talk(text);
             }
-            return Success("said: " + args.str);
+            return Success("said: " + text);
         }
     }
 }
