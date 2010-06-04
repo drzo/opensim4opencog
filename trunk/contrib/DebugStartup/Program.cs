15e2e9e3aee7f6ac3b043902c2c370e630b64daa
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-06-04 17:40:52 +0000

    COGBOT (ABuildStarup.exe) can be started with the option --aiml to debug just the HMM

--- trunk/contrib/DebugStartup/Program.cs
+++ trunk/contrib/DebugStartup/Program.cs
@@ -12,13 +12,26 @@ namespace DebugStartup
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            args = args ?? new string[0];
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
             Application.ThreadException += HandleThreadException;
             AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
+            if (args.Length > 0)
+            {
+                try
+                {
+                    RTParser.RTPBot.Main(args);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("" + e);
+                }
+                return;
+            }
             RadegastInstance instance = RadegastInstance.GlobalInstance;
             Application.Run(instance.MainForm);
             instance = null;
