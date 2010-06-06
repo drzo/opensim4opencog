2c311b6c269459499d2fc2cb42477bdbd49bc832
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-06-06 00:53:54 +0000

    Unifiable - broke Appendable version into its own files.. since its growing too big

---
+++ trunk/RTParser/Unify/StringAppendableUnifiable.cs
@@ -0,0 +1,64 @@
+using java.io;
+
+namespace RTParser
+{
+    public class StringAppendableUnifiable : StringUnifiable
+    {
+        public override Unifiable Frozen()
+        {
+            return "" + ToValue();
+        }
+
+        public StringAppendableUnifiable()
+        {
+            IsAppendable = true;
+        }
+
+        public override void Clear()
+        {
+            str = "";
+        }
+
+        public override string AsString()
+        {
+            return str.Trim().Replace("  ", " ");
+        }
+
+        public override void Append(Unifiable p)
+        {
+            if (!IsAppendable)
+            {
+                throw new InvalidObjectException("this " + AsString() + " cannot be appended with " + p);
+            }
+            if (Unifiable.IsNullOrEmpty(p)) return;
+            if (str == "")
+                str = p.AsString().Trim();
+            else
+            {
+                p = p.Trim();
+                if (!NoSpaceAfter(str) && !NoSpaceBefore(p))
+                    str += " ";
+                else
+                {
+                    str = str;
+                    str += " ";
+                }
+                str += p.AsString().Trim();
+                str = str.Replace("  ", " ");
+            }
+        }
+
+        private bool NoSpaceAfter(string str)
+        {
+            if (str.EndsWith("\"")) return true;
+            if (str.EndsWith("\'")) return true;
+            return false;
+        }
+        private bool NoSpaceBefore(string str)
+        {
+            if (str.StartsWith("\"")) return true;
+            if (str.StartsWith("\'")) return true;
+            return false;
+        }
+    }
+}
