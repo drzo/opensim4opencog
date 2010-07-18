bfd4484f87c9f61652265d79a7fbcfb6fc32155b
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-07-18 19:42:02 +0000

    RTPARSER - bot guard experiment with <when> tag

--- trunk/RTParser/AIMLTagHandlers/srai.cs
+++ trunk/RTParser/AIMLTagHandlers/srai.cs
@@ -49,7 +49,7 @@ namespace RTParser.AIMLTagHandlers
             {
                 base.writeToLog(unifiable, objs);
             }
-            if (string.Format(unifiable,objs).Contains("&"))
+            if (false && string.Format(unifiable,objs).Contains("&"))
             {
                 base.writeToLog("!ERRROR");
             }
@@ -71,15 +71,14 @@ namespace RTParser.AIMLTagHandlers
             return string.Empty;
         }
 
-        private static int depth = 0;
+        static int depth = 0;
         protected override Unifiable ProcessChange()
         {
             try
             {
-                depth++;
                 int d = request.GetCurrentDepth();
                 object prefix = string.Format("{0}: SRAI({1}/{2})", request.Graph, depth, d);
-                if (d > 30)
+                if (d > request.SraiDepth.Max)
                 {
                     writeToLog(prefix + "WARNING Depth pretty deep " + templateNode + " returning empty");
                     return Unifiable.Empty;
@@ -91,43 +90,18 @@ namespace RTParser.AIMLTagHandlers
                 }
                 if (this.templateNode.Name.ToLower() == "srai")
                 {
-                    if (!Unifiable.IsNull(RecurseResult))
+                    Unifiable templateNodeInnerValue = Recurse();
+                    if (!templateNodeInnerValue.IsEmpty)
                     {
-                        return RecurseResult;
-                    }
-                    if (!templateNodeInnerText.IsEmpty)
-                    {
-                        Unifiable tempTopic = GetAttribValue("topic", request.Topic);
-                        String gn = GetAttribValue("graph", null);
-
-                        Unifiable templateNodeInnerValue = Recurse();
-                        if (templateNodeInnerValue.IsEmpty)
-                        {
-                            writeToLog("InnerValue.IsEmpty! " + initialString);
-                            return templateNodeInnerValue;
-                        }
                         AIMLbot.Request subRequest = request.CreateSubRequest(templateNodeInnerValue, this.user,
                                                                               this.Proc, (AIMLbot.Request) request);
 
-                        
+
                         subRequest.Graph = request.Graph.Srai;
 
                         var ti = query.CurrentTemplate;
-                        if (ti!=null) subRequest.UsedTemplates.Add(ti);
+                        if (ti != null) subRequest.UsedTemplates.Add(ti);
 
-#if NOTUSINGAIML
-
-                        if (gn != null)
-                        {
-                            subRequest.Graph = Proc.GetGraph(gn, request.Graph);
-                        }
-                        depth = subRequest.depth = request.depth + 1;
-                        tempTopic = tempTopic.ToValue(query);
-                        subRequest.Topic = tempTopic;
-                        subRequest.ParentRequest = this.request;
-                        subRequest.StartedOn = this.request.StartedOn;
-                        subRequest.TimesOutAt = this.request.TimesOutAt;
-#endif
                         // make sure we don't keep adding time to the request
                         bool showDebug = true;
                         string subRequestrawInput = subRequest.rawInput;
@@ -139,11 +113,11 @@ namespace RTParser.AIMLTagHandlers
                         if (showDebug)
                             writeToLog(prefix + " CALLING '" + subRequestrawInput + "'");
 
-                       
+
                         if (mybot.chatTrace)
                         {
-                           //  mybot.bot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth - 1, depth-1);
-                          // mybot.bot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth, depth);
+                            //  mybot.bot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth - 1, depth-1);
+                            // mybot.bot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth, depth);
                             //mybot.bot.writeChatTrace("\"S{0}\" -> \"SRC:{1}\" ;\n", depth, CatTextInfo());
 
                             //mybot.bot.writeChatTrace("\"S{0}\" -> \"S{1}\" ;\n", depth - 1, depth);
@@ -183,9 +157,11 @@ namespace RTParser.AIMLTagHandlers
                                 mybot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth, depth);
                                 mybot.writeChatTrace("\"S{0}\" -> \"SIN:{1}\" ;\n", depth, subRequestrawInput);
                                 //mybot.writeChatTrace("\"SIN:{0}\" -> \"LN:{1}\" ;\n", subRequestrawInput, CatTextInfo());
-                                mybot.writeChatTrace("\"SIN:{0}\" -> \"PATH:{1}\" [label=\"{2}\"] ;\n", subRequestrawInput, depth,subResult.NormalizedPaths);
-                                mybot.writeChatTrace("\"PATH:{0}\" -> \"LN:{1}\" [label=\"{2}\"] ;\n", depth, depth, LineNumberTextInfo());
-                                
+                                mybot.writeChatTrace("\"SIN:{0}\" -> \"PATH:{1}\" [label=\"{2}\"] ;\n",
+                                                     subRequestrawInput, depth, subResult.NormalizedPaths);
+                                mybot.writeChatTrace("\"PATH:{0}\" -> \"LN:{1}\" [label=\"{2}\"] ;\n", depth, depth,
+                                                     LineNumberTextInfo());
+
                                 mybot.writeChatTrace("\"LN:{0}\" -> \"RPY:MISSING({1})\" ;\n", depth, depth);
                             }
 
@@ -196,7 +172,8 @@ namespace RTParser.AIMLTagHandlers
                             string sss = result.ToString();
                             if (showDebug)
                             {
-                                writeToLog("{0} SUCCESS RETURN {1}  {2} '{3}'", prefix, subRequestrawInput, subResult.Score, subQueryRawOutput);
+                                writeToLog("{0} SUCCESS RETURN {1}  {2} '{3}'", prefix, subRequestrawInput,
+                                           subResult.Score, subQueryRawOutput);
                                 if (query != null)
                                 {
                                     if (query.CurrentTemplate != null)
@@ -216,11 +193,21 @@ namespace RTParser.AIMLTagHandlers
                         {
                             mybot.writeChatTrace("\"L{0}\" -> \"S{1}\" ;\n", depth, depth);
                             mybot.writeChatTrace("\"S{0}\" -> \"SIN:{1}\" ;\n", depth, subRequestrawInput);
-                            mybot.writeChatTrace("\"SIN:{0}\" -> \"PATH:{1}\" [label=\"{2}\"] ;\n", subRequestrawInput, depth, subResult.NormalizedPaths);
-                            mybot.writeChatTrace("\"PATH:{0}\" -> \"LN:{1}\" [label=\"{2}\"] ;\n", depth, depth, LineNumberTextInfo());
+                            mybot.writeChatTrace("\"SIN:{0}\" -> \"PATH:{1}\" [label=\"{2}\"] ;\n", subRequestrawInput,
+                                                 depth, subResult.NormalizedPaths);
+                            mybot.writeChatTrace("\"PATH:{0}\" -> \"LN:{1}\" [label=\"{2}\"] ;\n", depth, depth,
+                                                 LineNumberTextInfo());
                             mybot.writeChatTrace("\"LN:{0}\" -> \"RPY:{1}\" ;\n", depth, subQueryRawOutput);
                         }
                         return subResult.Output;
+                    }
+                    else
+                    {
+                        if (templateNodeInnerValue.IsEmpty)
+                        {
+                            writeToLog("InnerValue.IsEmpty! " + initialString);
+                            return templateNodeInnerValue;
+                        }
                     }
                 }
                 return Unifiable.Empty;
