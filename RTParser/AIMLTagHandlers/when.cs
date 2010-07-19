f2c57d4796a447a9382107a7c38d2f556bd28c2c
logicmoo <logicmoo@gmail.com>
2010-07-19 03:11:35 +0000

    RTPARSER - Removed unused imports from <when> PROLOG - working around string.Format issues

--- RTParser/AIMLTagHandlers/when.cs
+++ RTParser/AIMLTagHandlers/when.cs
@@ -1,12 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using System.Xml;
-using DevExpress.XtraEditors;
 using RTParser.Utils;
 
 namespace RTParser.AIMLTagHandlers
