3a8a6694fdaf464ddbec6ce462d79ecff2304f75
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-08-02 07:55:57 +0000

    RTPARSER - PASSED="True" TESTCASE="javascript (19)" GOOD="1" RESP="Test case #19 Test passed" EXPECT="Test case #19 Test passed" INPUT="testjavascript" DESC="Tests the javascript element."

--- trunk/RTParser/AIMLTagHandlers/javascript.cs
+++ trunk/RTParser/AIMLTagHandlers/javascript.cs
@@ -1,6 +1,11 @@
 using System;
 using System.Xml;
 using System.Text;
+using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.JScript;
+
 
 namespace RTParser.AIMLTagHandlers
 {
@@ -30,8 +35,91 @@ namespace RTParser.AIMLTagHandlers
 
         protected override Unifiable ProcessChange()
         {
+            string s = templateNodeInnerText = Recurse();
+            if (GetAttribValue("onClient","False").StartsWith("T"))
+            {
+                writeToLogWarn("javascript tag is happeming on client: " + s);
+                return templateNode.OuterXml;
+            }
+#if NOJAVASCRIPT
             writeToLog("The javascript tag is not implemented in this Proc");
-            return Recurse();
+#else
+            try
+            {
+                s = "" + Evaluator.EvalToObject(templateNodeInnerText);
+            }
+            catch (Exception e)
+            {
+                writeToLog("ERROR: " + e + " caused by " + s);
+            }
+#endif
+            return s;
         }
     }
+#if !(NOJAVASCRIPT)
+    public class Evaluator
+    {
+        public static int EvalToInteger(string statement)
+        {
+            string s = EvalToString(statement);
+            return int.Parse(s.ToString());
+        }
+
+        public static double EvalToDouble(string statement)
+        {
+            string s = EvalToString(statement);
+            return double.Parse(s);
+        }
+
+        public static string EvalToString(string statement)
+        {
+            object o = EvalToObject(statement);
+            return o.ToString();
+        }
+
+        public static object EvalToObject(string statement)
+        {
+            return _evaluatorType.InvokeMember(
+                        "Eval",
+                        BindingFlags.InvokeMethod,
+                        null,
+                        _evaluator,
+                        new object[] { statement }
+                     );
+        }
+
+        static Evaluator()
+        {
+            ICodeCompiler compiler;
+            compiler = new JScriptCodeProvider().CreateCompiler();
+
+            CompilerParameters parameters;
+            parameters = new CompilerParameters();
+            parameters.GenerateInMemory = true;
+
+            CompilerResults results;
+            results = compiler.CompileAssemblyFromSource(parameters, _jscriptSource);
+
+            Assembly assembly = results.CompiledAssembly;
+            _evaluatorType = assembly.GetType("Evaluator.Evaluator");
+
+            _evaluator = Activator.CreateInstance(_evaluatorType);
+        }
+
+        private static object _evaluator = null;
+        private static Type _evaluatorType = null;
+        private static readonly string _jscriptSource =
+
+            @"package Evaluator
+            {
+               class Evaluator
+               {
+                  public function Eval(expr : String) : String 
+                  { 
+                     return eval(expr); 
+                  }
+               }
+            }";
+    }
+#endif
 }
