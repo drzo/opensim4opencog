bf96d5307c6006e51f41e6939b034618c44403ce
logicmoo@gmail.com <logicmoo@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2010-08-03 13:41:40 +0000

    LIBOMV - Needed to be able to produce a worthwhile "appearance packet to edit"

--- trunk/lib/OpenMetaverse/AppearanceManager.cs
+++ trunk/lib/OpenMetaverse/AppearanceManager.cs
@@ -1623,6 +1623,13 @@ namespace OpenMetaverse
         /// </summary>
         private void RequestAgentSetAppearance()
         {
+            AgentSetAppearancePacket set = MakeAppearancePacket();
+            Client.Network.SendPacket(set);
+            Logger.DebugLog("Send AgentSetAppearance packet");
+        }
+
+        public AgentSetAppearancePacket MakeAppearancePacket()
+        {
             AgentSetAppearancePacket set = new AgentSetAppearancePacket();
             set.AgentData.AgentID = Client.Self.AgentID;
             set.AgentData.SessionID = Client.Self.SessionID;
@@ -1772,9 +1779,7 @@ namespace OpenMetaverse
 
                 #endregion Agent Size
             }
-
-            Client.Network.SendPacket(set);
-            Logger.DebugLog("Send AgentSetAppearance packet");
+            return set;
         }
 
         private void DelayedRequestSetAppearance()
