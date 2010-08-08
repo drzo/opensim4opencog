de2d160a1f806ce3772d0088d713e16d22ec8e90
logicmoo <logicmoo@gmail.com>
2010-08-08 08:10:41 +0000

    AIML-IN-PROLOG - Separated out loader (and implemented!)

--- bin/cynd/logicmoo_module_aiml.pl
+++ bin/cynd/logicmoo_module_aiml.pl
@@ -15,6 +15,8 @@
 :- style_check(-atom).
 :- style_check(-string).
 
+alldiscontiguous:-!.
+
 :-multifile(what/3).
 :-multifile(response/2).
 
@@ -27,7 +29,7 @@ run_chat_tests:-
 test_call(G):-writeln(G),ignore(once(catch(G,E,writeln(E)))).
 
 main_loop1(Atom):- current_input(In),!,
-            read_line_with_nl(In,Codes,[]),!,
+            read_line_to_codes(In,Codes),!,
             atom_codes(Atom,Codes),!,
             alicebot(Atom),!.
 
@@ -73,14 +75,12 @@ alicebot(Input):-
    alicebot(Input,Resp),
    say(Resp).
 alicebot(Input):-say('-no response-').
-   
+
+
+atomSplit(A,B):-cyc:atomSplit(A,B).
 
 removePMark(UCase,Atoms):-append(AtomsPre,[Last],UCase),member(Last,[?,('.'),(',')]),!,removePMark(AtomsPre,Atoms).
 removePMark(Atoms,Atoms).
-
-
-
-%clean_out_atom(X,X).
 
 randomPick(List,Ele):-length(List,Len),Pick is random(Len),nth0(Pick,List,Ele),!.
 
@@ -202,6 +202,8 @@ set_matchit([_,Input|_],[_,Input|_]).
 %get_aiml_what(What,Match,OOut):-get_aiml_cyc(What,Match,Out),(([srai(Out)] = OOut);OOut=Out).
 get_aiml_what(What,Match,Out):-what(What, Match,Out).
 get_aiml_what([*],Match,Out):-response(Match,Out).
+get_aiml_what([A|L],Match,Out):-aimlCate(AIML),member(that=[A|L],AIML),member(pattern=Match,AIML),member(template=Out,AIML).
+get_aiml_what([*],Match,Out):-aimlCate(AIML),member(that=(*),AIML),member(pattern=Match,AIML),member(template=Out,AIML).
 
 %get_aiml_cyc([*],[String|ListO],[Obj,*]):-poStr(Obj,[String|List]),append(List,[*],ListO).
 %get_aiml_cyc([*],[String,*],[Obj,*]):-poStr(Obj,String).
@@ -275,201 +277,14 @@ degrade(_-OR):-!,degrade(OR).
 degrade(OR):-asserta(degraded(OR)).
    
 
-% =================================================================================
-% AIML Loading
-% =================================================================================
-load_aiml_files:-load_aiml_files('*.aiml'),!.
+:-['logicmoo_module_aiml_loader.pl'].
 
-load_aiml_files([]).
-load_aiml_files([H|T]):-
-   load_aiml_files(H),!,
-   load_aiml_files(T).
-load_aiml_files(F):-atom(F),
-   expand_file_name(F,[F]),!,
-   load_aiml_file(F).
-load_aiml_files(F):-atom(F),expand_file_name(F,FILES),!,load_aiml_files(FILES).
+%:-['bootstrap.aiml.pl'].
 
-aiml_files(F,Files):-atom_concat(F,'/',WithSlashes),expand_file_name(WithSlashes,WithOneSlash,[relative_to('./')]),
-                    atom_concat(WithOneSlash,'/*.aiml',Mask),expand_file_name(Mask,Files).
-
-load_aiml_file(F):- exists_directory(F), aiml_files(F,Files),!,load_aiml_files(Files).
-load_aiml_file(F):- file_name_extension(F,'aiml',Aiml).
-
-create_aiml_file(F):-
-create_aiml_file(F):-
-   atom_concat(F,'.pl',PLNAME),
-   time_file(PLNAME,PLTime), % fails on non-existent
-   time_file(F,FTime),
-   PLTime > FTime,!,
-   [PLNAME],!.
-
-create_aiml_file(F):-
-   atom_concat(F,'.pl',PLNAME),
-   tell(PLNAME),
-   (format(user_error,'%~w~n',[F])),
-   load_structure(F,X,[space(remove)]),!,
-   load_aiml_structure(notopic,X),!,
-   told,[PLNAME],!.
-
-
-load_aiml_structure(_,O):-ignore_aiml(O),!.
-load_aiml_structure(N,[X|L]):-load_aiml_structure(N,X),!,load_aiml_structure(N,L),!.
-   
-load_aiml_structure(N,element(aiml,L,T)):-!,load_aiml_structure(N,T).
-load_aiml_structure(N,element(a,L,T)):-!,load_aiml_structure(N,T).
-load_aiml_structure(N,element(substitute,[name=Catalog, find=Find, replace=Replace],[])):-!,
-	 load_aiml_structure(N,substitute(Catalog,Find,Replace)).
-load_aiml_structure(N,element(substitute,[name=Catalog, find=Find, replace=Replace],Stuff)):-!,
-	 load_aiml_structure(N,substitute(Catalog,Find,Replace,Stuff)).
-load_aiml_structure(N,element(topic,[name=Name],T)):-!,load_topic(Name,T),!.
-
-load_topic(_,L):-ignore_aiml(L).
-load_topic(Name,[H|T]):-
-      load_aiml_structure(Name,H),!,
-      load_topic(Name,T).
-
-load_aiml_structure(N,element(category,[],T)):-!,load_category(N,T),!.
-load_aiml_structure(N,element(startup,_,T)):-!.
-
-load_aiml_structure(N,substitute(Dict,Find,Replace)):-
-      atomSplit(Find,F),atomSplit(Replace,Resp),!,
-      load_aiml_structure(N,dict(Dict,F,Resp)).
-
-load_aiml_structure(N,X):-format('~q.~n',[X]).
-%      saveFAttribute(F,A),
-     % 
-      %(catch(X,_,fail);asserta(X)),!.
-
-:-dynamic(saveDFAttribute/2).
-:-dynamic(replace_t/5).
-:-dynamic(response_t/5).
-                
-saveFAttribute(F,A):-saveDFAttribute(F,A),!.
-saveFAttribute(F,A):-asserta(saveDFAttribute(F,A)),dynamic(F/A).
-
-% ===============================================================================================
-% ===============================================================================================
-
-ignore_aiml([]).
-ignore_aiml(A):-atom(A),atom_codes(A,C),clean_codes(C,[]).
-
-%clean_codes(X,X).
-
-
-aiml_classify([],[]).
-aiml_classify(Find,[atom]):-atomic(Find).
-aiml_classify([H|T],Out):-
-      classifySingle(H,Class),
-      aiml_classify(T,More),
-      sort([Class|More],OutM),!,
-      classify2(OutM,Out).
-aiml_classify(T,[unk]).
-
-classify2([in,out|Resp],[out|Resp]).
-classify2(Out,Out).
-
-classifySingle('_',var('_')).
-classifySingle(*,var('*')).
-classifySingle(Atom,in):-atom(Atom),all_upper_atom(Atom).
-classifySingle(Atom,out):-atom(Atom).
-classifySingle(Atom,spec(F)):-compound(Atom),functor(Atom,F,_).
-classifySingle(Atom,unknown).
-
-                                        
-      
-varize(Find,Replace,FindO,ReplaceO):-
-      subst((Find,Replace),'_','$VAR'(0),(FindM,ReplaceM)),
-      subst((FindM,ReplaceM),'*','$VAR'(0),(FindO,ReplaceO)),!.
-      
-
-load_category(_,L):-ignore_aiml(L).
-load_category(N,L):-
-      ignore(member(element(that,IA,IB),L)),
-      ignore((member(element(justbeforethat,[],[]),L),(IA= [index='1,2'],IB=[]))),
-      ignore(member(element(pattern,PA,PB),L)),
-      ignore(member(element(template,TA,TB),L)),!,
-      load_category(N,IA,IB,PA,PB,TA,TB),!.
-
-load_category(Topic,IA,IB,PA,PB,TA,TB):-
-      ignore(ignore_aiml(IA)),
-      convert_template(IB,I),
-      convert_template(PB,P),
-      convert_template(TB,T),
-      parse_category(Topic,IA,I,PA,P,TA,T).
-load_category(Topic,IA,IB,PA,PB,TA,TB):-trace.
-
-parse_category(notopic,[],[],[],PB,[],TB):-!,
-      load_aiml_structure(N,response(PB,TB)).
-
-parse_category(notopic, [],What, [], A, [], B):-!,
-      load_aiml_structure(N,what(What,A,B)).
-
-parse_category(Topic,[],[],[],PB,[],TB):-!,
-   load_aiml_structure(N,topic_resp(Topic,PB,TB)).
-
-parse_category(Topic,IA,IB,PA,PB,TA,TB):-!,
-   load_aiml_structure(N,category(Topic,IA,IB,PA,PB,TA,TB)).
-      
-
-
-convert_template(Input,Out):-
-   convert_template1(Input,M),
-   flatten(M,MM),
-   convert_template1(MM,O),
-   flatten(O,Out).
-
-
-convert_template1([],[]).
-convert_template1([I|P],L):-
-      ignore_aiml(I),!,convert_template1(P,L).
-convert_template1([I|P],[O|L]):-
-      convert_element(I,O),!,
-      convert_template1(P,L).
-convert_template1(P,P).
-
-      
-convert_element(Input,Out):-convert_ele(Input,M),!,convert_ele(M,Out),!.
-
-
-convert_ele(li(A),li(AA)):-convert_template(A,AA).
-convert_ele(get(A,[]),get(AA)):-convert_template(A,AA).
-convert_ele(element(set, [name=N], V),set(N,VV)):-convert_template(V,VV).
-convert_ele(element(get, [name=N], V),get(N,VV)):-convert_template(V,VV).
-convert_ele(element(SET_ATOM, [], V),set(N,VV)):-atom_concat('set_',N,SET_ATOM),convert_template(V,VV).
-convert_ele(element(GET_ATOM, [], V),get(N,VV)):-atom_concat('get_',N,GET_ATOM),convert_template(V,VV).
-convert_ele(element(BOT_ATOM, [], V),match(N,VV)):-atom_concat('bot_',N,BOT_ATOM),convert_template(V,VV).
-convert_ele(element(bot, [N=V], []),N=V).
-convert_ele(element(srai, [], B),srai(BB)):-convert_template(B,BB).
-convert_ele(element(random, [], B),random(BB)):-convert_template(B,BB).
-convert_ele(element(li, [], B),li(BB)):-convert_template(B,BB).
-convert_ele(element(star, [], []),(*)).
-convert_ele(element(a, [Target, Link], Name),A):-sformat(S,'<a ~q ~q>~w</a>',[Target, Link, Name]),string_to_atom(S,A).
-convert_ele(element(a, [Link], Name),A):-sformat(S,'<a ~q>~w</a>',[Link, Name]),string_to_atom(S,A).
-
-convert_ele(element(A, [], []),A):-!.
-convert_ele(element(A, [], B),T):-!,convert_template(B,BB), T=..[A,BB].
-convert_ele(element(A, B, []),T):-!,convert_template(B,BB), T=..[attrib,A,BB].
-convert_ele(element(A, B, C),T):-!,
-      convert_template(B,BB),
-      convert_template(C,CC),!, 
-      T=..[A,BB,CC].
-convert_ele(attrib(get, name=Var),get(Var)):-!.
-convert_ele(attrib(learn, filename=F),load_any_file(F)):-!.
-convert_ele(sr,[srai([*])]).
-convert_ele(star,*).
-
-convert_ele(L,LO):-is_list(L),flatten(L,M),!,
-	    (L==M -> LO=M ; convert_template(M,LO)).
-
-convert_ele(A,W):-atom(A),clean_out_atom(A,B),
-	    atomSplit(B,W).
-convert_ele(O,O).
-
-
-:-load_aiml_files.
-
+%:-load_aiml_files.
 
 %:-debug,run_chat_tests.
 
-:-main_loop.
+%:-main_loop.
+
 
