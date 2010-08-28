fab0fb75d8610c81c2c66fa1fd9ad11786d941bb
logicmoo <logicmoo@gmail.com>
2010-08-28 20:55:25 +0000

    PREBUILD - added Lucene.Net and ABuildStartup32

---
+++ contrib/DebugStartup32/Program32.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ABuildStartup
+{
+
+    public class Program32
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+			Program.Main();
+		}
+    }
+}
