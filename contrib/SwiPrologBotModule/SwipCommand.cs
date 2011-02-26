185e9c1455834d571561b9503898d86ece898ba0
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2011-02-26 02:28:29 +0000

    PROLOG - added swip command for testing for testing prolog interpretor

---
+++ contrib/SwiPrologBotModule/SwipCommand.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+
+using MushDLR223.ScriptEngines;
+
+namespace cogbot.Actions.System
+{
+    public class SwipCommand : Command, RegionMasterCommand
+    {
+        private PrologScriptEngine.PrologScriptInterpreter pse = null;
+		public SwipCommand(BotClient testClient)
+        {
+            Name = "swip";
+            Description = "runs swi-prolog commands on current sim.";
+            Category = CommandCategory.Simulator;
+            Parameters = new[] { new NamedParam(typeof(GridClient), null), new NamedParam(typeof(string), null) };
+        }
+
+        public override CmdResult acceptInput(string verb, Parser args, OutputDelegate WriteLine)
+        {
+            int argsUsed;
+            string text = args.str;
+
+            return Success("swip is " + text);
+        }
+    }
+}
