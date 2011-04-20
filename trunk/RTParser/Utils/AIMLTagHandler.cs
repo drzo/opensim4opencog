c2d99c5c91b8903d2b345339df22898295d99b6c
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2011-04-20 01:04:25 +0000

    AIMLBOT - do not do a ChatSignalOverBudget on C# depth! maybe one AIML depth

--- trunk/RTParser/Utils/AIMLTagHandler.cs
+++ trunk/RTParser/Utils/AIMLTagHandler.cs
@@ -549,8 +549,8 @@ namespace RTParser.Utils
             bool success = true;
             if (vv == null) return false;
             if (IsNullOrEmpty(vv)) return false;
-            if (DLRConsole.IsTooDeep())
-                throw new ChatSignalOverBudget(childHandler.request, "CompleteEvaluatution: too Deep " + vv);
+            if (DLRConsole.IsTooDeep()) ;
+            //    throw new ChatSignalOverBudget(childHandler.request, "CompleteEvaluatution: too Deep " + vv);
             if (!IsUnevaluated(vv))
             {
                 output = vv;
