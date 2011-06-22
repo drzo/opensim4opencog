dfd19934fe496d68f8c87093c890a4633fc0be28
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2011-06-22 09:07:37 +0000

    SWIPLCS - works on both Linux and Windows now

---
+++ sources/main/swicli/swicli.c
@@ -0,0 +1,104 @@
+#if defined(_MSC_VER) && !defined(WINDOWS_CPP)
+#define WINDOWS_CPP 1
+#endif
+
+#ifndef WINDOWS_CPP
+#include <mono/jit/jit.h>
+#include <mono/metadata/environment.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/mono-config.h>
+#include <stdlib.h>
+#include <string.h>
+#endif //WINDOWS_CPP
+
+#include "SWI-Prolog.h"
+#include "stdafx.h"
+
+/*
+
+Linux: Compile with: 
+ 	  ../../../bin/swipl-ld -shared -o ../../../bin/swicli.so swicli.c `pkg-config --cflags --libs mono-2` -lm
+
+Windows: remember "Not Using Precompiled Headers"
+          and compile this file as .cpp
+
+*/
+
+#ifdef WINDOWS_CPP
+extern "C" {
+#endif //WINDOWS_CPP
+
+	/// static MonoString* gimme () {   	return mono_string_new (mono_domain_get (), "All your monos are belong to us!"); }
+
+	/// ?- load_foreign_library(swicli).
+	/// Assembly + ClassName + StaticMethodName
+	/// Such as  ?- cli_load_lib('SwiPlCs.dll','SbsSW.SwiPlCs.swipl_win','install').
+	foreign_t  cli_load_lib(term_t aname, term_t cname, term_t mname) 	
+	{ 
+		char *anamestr;
+		char *cnamestr;
+		char *mnamestr;
+		if ( PL_get_atom_chars(aname, &anamestr) && PL_get_atom_chars(cname, &cnamestr) && PL_get_atom_chars(mname, &mnamestr) )
+		{
+#ifdef WINDOWS_CPP
+			System::Reflection::Assembly^ assembly = System::Reflection::Assembly::Load(gcnew System::String(anamestr));
+			System::Type^ type = assembly->GetType(gcnew System::String(cnamestr));
+			System::Reflection::MethodInfo^ method = type->GetMethod(gcnew System::String(mnamestr));
+			method->Invoke(nullptr, gcnew cli::array<System::Object^,1>(0));
+#else
+			/*
+			* Load the default Mono configuration file, this is needed
+			* if you are planning on using the dllmaps defined on the
+			* system configuration
+			*/
+			mono_config_parse (NULL);
+
+			/*  mono_jit_init() creates a domain: each assembly is loaded and run in a MonoDomain. */
+			MonoDomain* domain = mono_jit_init (anamestr);
+
+			/* add our special internal call, so that C# code can call us back. */
+			//mono_add_internal_call ("MonoEmbed::gimme", gimme);
+
+			MonoAssembly* assembly = mono_domain_assembly_open (domain, anamestr);
+			if (!assembly) return PL_warning("No assembly anamestr %s", anamestr);
+
+			MonoImage* image = mono_assembly_get_image(assembly);
+			if (!image) return PL_warning("No image anamestr %s", anamestr);
+
+			char str[255];
+			strcpy(str,cnamestr);
+			strcat(str,":");
+			strcat(str,mnamestr);
+			strcat(str,"()");
+
+			MonoMethodDesc* desc = mono_method_desc_new(str,0);
+			if (!desc) return PL_warning("No method desc %s", str);
+
+			MonoMethod* method = mono_method_desc_search_in_image(desc,image);
+			if (!method) return PL_warning("No method %s", str);
+
+			void *args[0];
+			mono_runtime_invoke(method, NULL, args, NULL);
+
+			// we exit from our host app
+			//retval = mono_environment_exitcode_get ();
+			//mono_jit_cleanup (domain);
+
+#endif //WINDOWS_CPP
+
+			PL_succeed;
+		}
+		PL_fail;
+	}
+
+	// install_t == __declspec( dllexport )
+	install_t install()
+	{
+		PL_register_foreign("cli_load_lib", 3, cli_load_lib, 0);
+	}
+
+#ifdef WINDOWS_CPP
+}
+#endif //WINDOWS_CPP
+
