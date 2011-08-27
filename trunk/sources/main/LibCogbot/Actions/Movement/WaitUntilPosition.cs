d5571db10b735caf9389a83f733a0be4afa3f7a5
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2011-08-27 17:40:50 +0000

    COGBOT - added WaitUntilPosition command

---
+++ trunk/sources/main/LibCogbot/Actions/Movement/WaitUntilPosition.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading;
+using OpenMetaverse;
+using PathSystem3D.Navigation;
+
+using MushDLR223.ScriptEngines;
+
+namespace cogbot.Actions.Movement
+{
+
+    public class WaitUntilPosition : cogbot.Actions.Command, BotPersonalCommand
+    {
+        public WaitUntilPosition(BotClient client)
+        {
+            Name = "waitpos";
+            Description = "Block until the robot gets to a certain position for a certain maxwait";
+            Category = cogbot.Actions.CommandCategory.Movement;
+            Usage = "waitpos seconds <x,y,z>";
+            Parameters = new[]
+                             {
+                                 new NamedParam("seconds", typeof (TimeSpan), typeof (float)),
+                                 new NamedParam("position", typeof (SimPosition), typeof (Vector3d))
+                             };
+
+        }
+
+        public override CmdResult Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            if (args.Length < 2)
+                return ShowUsage();
+
+            int argcount;
+            float maxSeconds;
+            if (!float.TryParse(args[0], out maxSeconds))
+            {
+                maxSeconds = -1;
+            }
+            else
+            {
+                args = Parser.SplitOff(args, 1);
+            }
+            SimPosition pos = WorldSystem.GetVector(args, out argcount);
+            if (pos == null)
+            {
+                return Failure(String.Format("Cannot {0} to {1}", Name, String.Join(" ", args)));
+            }
+
+            DateTime waitUntil = DateTime.Now.Add(TimeSpan.FromSeconds(maxSeconds));
+            float maxDistance = 1f;
+
+            bool MadIt = false;
+            while (!MadIt && waitUntil < DateTime.Now)
+            {
+                if (Vector3d.Distance(pos.GlobalPosition, TheSimAvatar.GlobalPosition) < (pos.GetSizeDistance() + 1))
+                {
+                    MadIt = true;
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            if (MadIt)
+            {
+                return Success(string.Format("SUCCESS {0}", str));
+
+            }
+            else
+            {
+                return Success("FAILED " + str);
+            }
+        }
+    }
+}
