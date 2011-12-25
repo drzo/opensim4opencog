b2e102c0cc005dcb7cd7794217ce75d0149b1e5c
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2011-12-25 00:26:10 +0000

    PREBUILD - chatbot people now must run include-chatbot.bat

---
+++ include-chatbot.bat
@@ -0,0 +1,10 @@
+
+
+svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/alicebot/aiml bin/aiml/
+svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/alicebot/config bin/config/
+svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/wordnet30 bin/wordnet30/
+svn co https://opensim4opencog.googlecode.com/svn/trunk-chatbot/sources/chatbot sources/chatbot/
+svn co https://opensim4opencog.googlecode.com/svn/trunk-chatbot/bin-chatbot bin-chatbot/
+svn co https://opensim4opencog.googlecode.com/svn/trunk-logicmoo/prolog/programk bin/prolog/programk
+xcopy /y /c /e bin-chatbot/*.* bin/
+
