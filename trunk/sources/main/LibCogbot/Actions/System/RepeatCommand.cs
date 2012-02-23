7cc7023bbf59b11daad55523071dc613016eba53
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-02-23 11:02:15 +0000

    COGBOT - added 'repeat command.. example:  repeat 30 jump ... to jump every 30 seconds

---
+++ trunk/sources/main/LibCogbot/Actions/System/RepeatCommand.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+
+using MushDLR223.ScriptEngines;
+
+namespace cogbot.Actions
+{
+    public class RepeatCommand : Command, BotSystemCommand
+    {
+        public RepeatCommand(BotClient testClient)
+        {
+            Name = "thread";
+            Description = "Repeats a command in its own thread. Usage: thread 30 jump";
+            Category = CommandCategory.BotClient;
+        }
+
+        public override CmdResult Execute(string[] args, UUID fromAgentID, OutputDelegate WriteLine)
+        {
+            //BotClient Client = TheBotClient;
+            if (args.Length < 2)
+            {
+                return ShowUsage();// " ";
+            }
+            int secondsOfSleep;
+            if (!int.TryParse(args[0], out secondsOfSleep))
+            {
+                return ShowUsage();
+            }
+            string[] newArgs = Parser.SplitOff(args, 1);
+            String cmd = String.Join(" ", args);
+            ThreadStart thread = () =>
+                                           {
+                                               try
+                                               {
+                                                   while (true)
+                                                   {
+                                                       try
+                                                       {
+                                                           var result = Client.ExecuteCommand(cmd, fromAgentID,
+                                                                                              WriteLine);
+                                                           if (result == null)
+                                                           {
+                                                               WriteLine("No command found! \"" + cmd + "\"");
+                                                               return;
+                                                           }
+                                                           else
+                                                           {
+                                                               WriteLine(result.ToString());
+                                                           }
+                                                       }
+                                                       catch (ThreadAbortException e)
+                                                       {
+                                                           WriteLine("Aborting " + cmd);
+                                                       }
+                                                       catch (Exception e)
+                                                       {
+                                                           WriteLine("Problem with " + cmd + " " + e);
+                                                           return;
+                                                       }
+                                                       Thread.Sleep(secondsOfSleep*1000);
+                                                   }
+                                               }
+                                               finally
+                                               {
+                                                   try
+                                                   {
+                                                           TheBotClient.RemoveThread(Thread.CurrentThread);
+                                                   }
+                                                   catch (OutOfMemoryException){}catch (StackOverflowException){}catch (Exception){}
+                                                   WriteLine("done with " + cmd);
+                                               }
+                                           };
+            String threadName = "Repeating " + cmd;
+            TheBotClient.InvokeThread(threadName, thread);
+            return Success(threadName);
+        }
+    }
+}
