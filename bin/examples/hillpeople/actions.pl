999f87722c0b3e9e78eab7af26998919f0cb8ef3
anneogborn <anneogborn@gmail.com>
2012-05-13 04:59:45 +0000

    Resolve between Doug and I both refactoring same way at same time.
    Keeping mine, essentially. Doug's changing default writeln delegate to /dev/null

--- bin/examples/hillpeople/actions.pl
+++ bin/examples/hillpeople/actions.pl
@@ -2,18 +2,24 @@
 		     botcmd_with_bot/2,
 		     call_with_bot/2,
 		     with_all_bots/1,
-                     hillPeopleCmd/1,
-                     hillPeopleCmd/2,
-                     hillPeopleSay/2
+		     say_format/2,
+		     say_ref/2
 		    ]).
 
 :- use_module(cogbot(cogrobot)).
 :- use_module(hillpeople(hillpeople)).
 :- use_module(hillpeople(navigation)).
 
-hillPeopleCmd(Cmd):-botClientCmd(Cmd,_).
-hillPeopleCmd(Cmd,Str):-botClientCmd(Cmd,Obj),cli_to_str(Obj,Str).
-hillPeopleSay(Fmt,Args):-format(string(S),Fmt,Args),hillPeopleCmd(say(S),_).
+say_format(Format, Args) :-
+    format(string(Contents), Format, Args),
+    format(string(QContents), '"~w"', [Contents]),
+    botClientCmd(say(QContents), _).
+
+say_ref(Prompt, Ref) :-
+    cli_to_str(Ref, S),
+    format(string(Out), '"~w: ~w"', [Prompt, S]),
+    botClientCmd(say(Out), _).
+
 
 % doug - is there a more elegant way than generating botcmd to
 % do setbot?
@@ -23,7 +29,7 @@ with_bot(Name, Goal) :-
 	cli_get(ID, name, SLName),
 	format(string(S), 'setbot ~w', [SLName]),
         string_to_atom(S, A),
-	hillPeopleCmd(A),
+	botClientCmd(A),
 	call(Goal). % in an ideal world I'd set it back,
                     %but can't find current bot
 */
