9c1ee05f10bd2c6a234fcf4a42fa0a9a996805d9
anneogborn@gmail.com <anneogborn@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-05-14 20:36:11 +0000

    Resolved conflict with Doug's edits

--- trunk/bin/examples/hillpeople/actions.pl
+++ trunk/bin/examples/hillpeople/actions.pl
@@ -1,7 +1,4 @@
 :- module(actions , [
-		     botcmd_with_bot/2,
-		     call_with_bot/2,
-		     with_all_bots/1,
 		     say_format/2,
 		     say_ref/2
 		    ]).
@@ -16,41 +13,9 @@ say_format(Format, Args) :-
     botcmd(say(QContents)).
 
 say_ref(Prompt, Ref) :-
-    cli_to_str(Ref, S),
+    (	cli_to_str(Ref, S) ; S = "Darn cli_to_str failed"),
     format(string(Out), '"~w: ~w"', [Prompt, S]),
     botcmd(say(Out)).
-
-
-% doug - is there a more elegant way than generating botcmd to
-% do setbot?
-/*
-with_bot(Name, Goal) :-
-	botID(Name, ID),
-	cli_get(ID, name, SLName),
-	format(string(S), 'setbot ~w', [SLName]),
-        string_to_atom(S, A),
-	botcmd(A),
-	call(Goal). % in an ideal world I'd set it back,
-                    %but can't find current bot
-*/
-
-botcmd_with_bot(Name, BotCmd) :-
-	botID(Name, BotID),
-	cli_call(BotID, 'ExecuteCommand'(BotCmd), _Ret).
-
-
-call_with_bot(_Name, _Goal).  % TODO :-
-/*	botID(Name, BotID),
-	cli_call(BotID, 'ExecuteCommand'('jump'), _Ret).
-*/
-
-%
-%  call goal on all bots
-%
-with_all_bots(Goal) :-
-	hill_person(Name),
-	call_with_bot(Name, Goal).
-
 
 
 
