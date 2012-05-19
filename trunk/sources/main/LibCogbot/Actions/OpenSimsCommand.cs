d80790cb4c560d37743d266786960bb168cc3ffb
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-05-19 04:40:16 +0000

    COGBOT - issue 50 .. consolidate TheOpenSims interface to one class to be loaded in TheSimsModule

--- trunk/sources/main/LibCogbot/Actions/OpenSimsCommand.cs
+++ trunk/sources/main/LibCogbot/Actions/OpenSimsCommand.cs
@@ -7,9 +7,63 @@ using System.Threading;
 using cogbot.Listeners; //using libsecondlife;
 
 using MushDLR223.ScriptEngines;
+using PathSystem3D.Navigation;
 
 namespace cogbot.Actions
 {
+    class Use : Command, BotPersonalCommand
+    {
+        public Use(BotClient Client)
+            : base(Client)
+        {
+            Description = "Interface to the OpenSims module. <a href='wiki/TheOpenSims'>Documentation Here</a>";
+            Usage = "DMILES TODO";
+            Category = CommandCategory.Objects;
+            Parameters = new[] { new NamedParam(typeof(SimPosition), typeof(UUID)) };  //DMILES TODO
+            // DMILE
+            Name = "Use..";
+        }
+
+        public override CmdResult acceptInput(string verb, Parser args, OutputDelegate WriteLine)
+        {
+            //   base.acceptInput(verb, args);
+            string to_op = "";
+            string objname = "";
+            if (args["to"].Length > 0)
+            {
+                to_op = args["to"];
+            }
+            objname = args.objectPhrase;
+            if (objname == "")
+            {
+                return Failure("$bot don't know what object to use.");
+            }
+            if (to_op == "")
+            {
+                SimObject objToUse;
+                if (WorldSystem.tryGetPrim(objname, out objToUse))
+                {
+                    if ((BotNeeds)TheSimAvatar["CurrentNeeds"] == null)
+                    {
+                        TheSimAvatar["CurrentNeeds"] = new BotNeeds(90.0f);
+                    }
+                    SimTypeUsage usage = objToUse.Affordances.GetBestUse((BotNeeds)TheSimAvatar["CurrentNeeds"]);
+                    if (usage == null)
+                    {
+                        //usage = new MoveToLocation(TheSimAvatar, objToUse);
+                        return Failure("$bot don't have a use for " + objToUse + " yet.");
+                    }
+                    TheSimAvatar.Do(usage, objToUse);
+                    return Success("used " + objToUse);
+                }
+                return Failure("$bot don't know what to do with " + objname);
+            }
+            WriteLine("Trying to (" + to_op + ") with (" + objname + ")");
+            TheBotClient.UseInventoryItem(to_op, objname);
+            return Success("completed to (" + to_op + ") with (" + objname + ")");
+
+        }
+    }
 
     class DoCommand : Command, BotPersonalCommand
     {
