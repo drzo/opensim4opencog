679b49368a4d327c4332958b2cea19a45a5f5374
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-05-20 19:33:13 +0000

    COGBOT - MoveToCommand uses autopilot

--- trunk/sources/main/LibCogbot/Actions/Movement/MoveToCommand.cs
+++ trunk/sources/main/LibCogbot/Actions/Movement/MoveToCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using OpenMetaverse;
 using PathSystem3D.Navigation;
 
@@ -6,7 +7,7 @@ using MushDLR223.ScriptEngines;
 
 namespace cogbot.Actions.Movement
 {
-    class MovetoCommand : Command, BotPersonalCommand
+    class MovetoCommand : Command, BotPersonalCommand, BotStatefullCommand
     {
         public MovetoCommand(BotClient client)
         {
@@ -28,10 +29,44 @@ namespace cogbot.Actions.Movement
                 return Failure("Coulnd not resolve location: " + string.Join(" ", args));
             }
             Vector3d g = position.GlobalPosition;
-            TheSimAvatar.SetMoveTarget(position, position.GetSizeDistance());
-            //Client.Self.AutoPilot(g.X, g.Y, g.Z);
-
+            var TheSimAvatar = this.TheSimAvatar;
+            TheSimAvatar.SetClient(TheBotClient);
+            //TheSimAvatar.SetMoveTarget(position, position.GetSizeDistance());
+            Client.Self.AutoPilot(g.X, g.Y, g.Z);
+            Dispose();
+           // MoveThread = new Thread(MoveProc);
             return Success(string.Format("SetMoveTarget: <{0},{1},{2}>", g.X, g.Y, g.Z));
         }
+
+        private Vector3d target = Vector3d.Zero;
+        private void MoveProc()
+        {
+            var TheSimAvatar = this.TheSimAvatar;
+            while (true)
+            {
+                if (target == Vector3d.Zero) return;
+                TheSimAvatar.StopMoving();
+                TheSimAvatar.TurnToward(target);
+            }
+        }
+
+        private Thread MoveThread;
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <filterpriority>2</filterpriority>
+        public void Dispose()
+        {
+            /*if (MoveThread == null) return;
+
+            if (MoveThread.IsAlive)
+            {
+                MoveThread.Abort();
+            }
+            MoveThread = null;
+        */
+        }
+
     }
 }
