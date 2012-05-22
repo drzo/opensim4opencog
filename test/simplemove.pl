4cd3c1c58fc2e206b7ed2be4423e8a2f40cc0304
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-05-22 21:55:52 +0000

    PROLOG - simplemove.pl uses support files

--- test/simplemove.pl
+++ test/simplemove.pl
@@ -27,7 +27,7 @@ get_test_waypoints(Name,loop([Name1,Name2,Name1])):-atom_concat(Name,1,Name1),at
 %
 %--------------------------------------------------------
 :-set_num_bots(2).
-:-set_tribe('Hillpeople').
+:-set_tribe('Hillperson').
 
 :-logon_bots.
 
