d35a03245cccb2899d96e825b96419e84bbc9beb
anneogborn@gmail.com <anneogborn@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-05-25 21:58:54 +0000

    Updated to reflect some changes in botCmd syntax - format_say has some weirdness, want to ask Douglas about it
    This version just ran 90 minutes with no problems

--- trunk/bin/examples/hillpeople/actions.pl
+++ trunk/bin/examples/hillpeople/actions.pl
@@ -8,14 +8,21 @@
 :- use_module(hillpeople(navigation)).
 
 say_format(Format, Args) :-
-    format(string(Contents), Format, Args),
-    format(string(QContents), '"~w"', [Contents]),
-    botcmd(say(QContents)).
+    'format'(string(Contents), Format, Args),
+%    'format'(string(QContents), '"~s"', [Contents]),
+    botcmd(say(Contents)).
+
+% DEBUG TODO
+%  Douglas - what's up here? why are we quoting, and why is it failing?
+say_format(Format, Args) :-
+	format(Format, Args),nl.
+say_format(Format, Args) :-
+	write('Format='),write(Format),nl,
+	write('Args='),write(Args),nl.
+say_format(_, _).
 
 say_ref(Prompt, Ref) :-
-    (	cli_to_str(Ref, S) ; S = "Darn cli_to_str failed"),
-    format(string(Out), '"~w: ~w"', [Prompt, S]),
-    botcmd(say(Out)).
-
+    once(cli_to_str(Ref, S) ; S = "Darn cli_to_str failed"),
+    say_format('~w = ~s', [Prompt, S]).
 
 
