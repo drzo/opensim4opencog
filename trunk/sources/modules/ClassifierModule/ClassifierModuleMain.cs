376f72be275001828933c63f0a757a22b997030f
anneogborn@gmail.com <anneogborn@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-06-05 21:03:16 +0000

    made it compile.

--- trunk/sources/modules/ClassifierModule/ClassifierModuleMain.cs
+++ trunk/sources/modules/ClassifierModule/ClassifierModuleMain.cs
@@ -10,6 +10,25 @@ namespace ClassifierModule
 {
     class ClassifierModuleMain : WorldObjectsModule
     {
+        public ClassifierModuleMain(BotClient _parent)
+            : base(_parent)
+        {
 
+        }
+
+        public override void Dispose()
+        {
+           // throw new NotImplementedException();
+        }
+
+        public override string GetModuleName()
+        {
+            return this.GetType().Namespace;
+        }
+
+        public override void StartupListener()
+        {
+           // throw new NotImplementedException();
+        }
     }
 }
