826133289499fc1e07c36b9139bb6a917a76d0c7
anneogborn <anneogborn@gmail.com>
2012-06-07 17:00:42 +0000

    nrrgh.... got 'already under control'

---
+++ bin/examples/hillpeople/weatherserver/climate.pl
@@ -0,0 +1,127 @@
+:- module(climate, [
+		    test_weather/0,
+		    is_night/0,
+		    berries_in_season/0,
+		    is_snowing/0,
+		    is_raining/0,
+		    hill_date/1
+		    ]).
+
+% The year is 12 months of 28 days, The year starts
+% April 1
+%
+%  by week, percentages (0 -100)
+% Rain-Snow probabilty of each
+precip([
+    25-7, 45-0, 50-0, 35-0,
+    20-0, 20-0, 20-0, 10-0,
+    15-0, 10-0, 3-0, 5-0,   % June
+    10-0, 15-0, 3-0, 3-0,
+    3-0, 3-0, 2-0, 5-0,
+    25-0, 25-0, 15-0, 35-0,  %september
+    20-0, 15-0, 2-0, 8-20,
+    2-25, 7-35, 3-55, 1-70,
+    0-55, 0-80, 0-80, 0-90,   % December
+    0-50, 0-40, 0-40, 0-65,
+    0-74, 7-40, 20-25, 25-45,
+    25-20, 15-40, 25-20, 25-20   % march
+
+	]).
+
+
+%
+%  one_days_weather
+%
+one_days_weather(Rain, _, rain) :-
+	RainProb is Rain / 100.0,
+	random(X),
+	X < RainProb,!.
+one_days_weather(_, Snow, snow) :-
+	SnowProb is Snow / 100.0,
+	random(X),
+	X < SnowProb,!.
+one_days_weather(_, _, clear) :- !.
+
+
+%
+%  weather
+%
+weather(Climate, Weather) :-
+	weather0(0, Climate, Weather).
+
+weather0(_, [], []).
+weather0(N, [Rain-Snow|T], [TodaysWeather|WeatherTail]) :-
+	N < 7,
+	one_days_weather(Rain, Snow, TodaysWeather),
+	ND is N + 1,
+	weather0(ND, [Rain-Snow|T], WeatherTail).
+weather0(7, [_|T], Weather) :-
+	weather0(0, T, Weather).
+
+month_names([
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    november,
+    december,
+    january,
+    february,
+    march
+	    ]).
+
+print_weather(28, [], []) :- !.
+print_weather(28, [HMonth|TMonth], W) :- !,
+	format('====== ~w =========~n', [HMonth]),
+	print_weather(0, TMonth, W).
+print_weather(N, Months, [Today|W]) :-
+	6 =:= N mod 7,!,
+	format('| ~w |~n', [Today]),
+	NN is N + 1,
+	print_weather(NN, Months, W).
+print_weather(N, Months, [Today|W]) :-
+	format('| ~w ', [Today]),
+	NN is N + 1,
+	print_weather(NN, Months, W).
+
+test_weather :-
+	climate(Climate),
+	weather(Climate, W),
+	month_names(Months),
+	print_weather(28,
+		      Months,
+		      W).
+
+% TODO
+is_night :- fail.
+
+%  OOoops !  Bot has to figure out date.
+%
+% TODO
+% note that this is just 'in season', not actually ripe
+berries_in_season :-
+	hill_date(Date),
+	berries_in_season(Date).
+
+berries_in_season(Date) :-
+	Month =:= Date / 28,
+	memberchk(Month, [5,6,7]).  % berries in season in june, july, aug
+
+% TODO
+is_snowing :- fail.
+
+% TODO
+is_raining :- fail.
+
+
+% TODO
+%
+% hill_date(?Date) unifies if the world time is
+% the int value (0-335) of the day in world
+%
+hill_date(180).
+
+
---
+++ bin/examples/hillpeople/weatherserver/weatherserver.pl
@@ -0,0 +1,79 @@
+:- module(weatherserver, [
+			  start/1
+			 ]).
+% Given that this file is called weatherserver.pl,
+% start the server by:
+%
+% swipl -g "[weatherserver], start(19888)."
+%
+% Usage:
+%
+% http://localhost:19888/weather?day=17.75&seed=218
+%
+% day is the day of the 336 day year, starting from 0 being April 1
+% of year 0, and fractional day. The day starts at sunrise.
+% seed is an arbitrary integer that controls which weather track
+% to return. It is modded into 0-255
+%
+% returns a single line CSV list of values. boolean values
+% are 0 for false and 1 for true. Values may be added to end,
+% and there should be a default if the list is short
+%
+% valid - if false, couldn't return a valid answer
+% sun  - current sun angle, in 24 hours
+% temp  - current centigrade temperature
+% fallen_snow  - amount of fallen snow
+% is_snowing  - it's currently snowing
+% is_raining  - it's currently raining
+%
+%
+%
+% http://localhost:8000/sum?summands=blah
+% returns <error>ERROR</error>
+
+:- use_module(library('http/thread_httpd')).
+:- use_module(library('http/http_dispatch')).
+:- use_module(library('http/http_parameters')).
+:- use_module(climate).
+
+:- http_handler('/weather', weather_handler, []).
+
+start(Port) :-
+    http_server(http_dispatch, [port(Port), workers(1)]),
+    thread_get_message(_),
+    halt.
+
+weather_handler(Request) :-
+    http_parameters(Request, [day(Day, [default(0)])]),
+    catch(
+        (
+            weather_for_day(
+		Day,
+		weather(
+		    Sun,
+		    Temperature,
+		    SnowAccum,
+		    Snowing,
+		    Raining)),
+            with_output_to(
+		atom(Output),
+		format("~f,~f,~f,~d,~d",
+		       [Sun, Temperature, SnowAccum, Snowing, Raining]))
+        ),
+        _,
+        Output = '0,1.0,1.0,0.0,0,0'
+    ),
+    format('Content-type: text/css\r\n\r\n~w', [Output]).
+
+temperature(Date, Temp) :-
+
+
+weather_for_day(Day,
+	weather(Sun, Temperature, SnowAccum, Snowing, Raining)) :-
+	Date =:= floor(Day),
+	Sun =:= 24.0 * (Date rem 1.0),
+	temperature(Date, Temperature),
+	snow_accumulation(Date, SnowAccum),
+	is_snowing(Date, Snowing),
+	is_raining(Date, Raining).
+
