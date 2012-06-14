c8cb28a64f0e1c30bc0779ee694def7122157b79
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-06-14 00:35:16 +0000

    COGBOT - New BotConfig Example

---
+++ bin/personalAccts.lisp
@@ -0,0 +1,8 @@
+
+
+    (create-botclient "ExampleBot" "Resident" "pass123" "agni" "uri:Neverdol&amp;152&amp;72&amp;1973")
+
+    ;;(create-botclient "Otopopo" "Dougstribe" "hillpeople" "http://www.pathwayslms.com:9000/" "last")
+
+
+
