7c9c01ade9a4a6cb0cf09f7f2e763787e7594519
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-07-01 19:38:12 +0000

    PREBUILD - We need updates to include-chatbot here for a while (workflow)

--- trunk/include-chatbot.bat
+++ trunk/include-chatbot.bat
@@ -3,7 +3,7 @@
 svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/alicebot/aiml bin/aiml/
 svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/alicebot/config bin/config/
 svn co https://opensim4opencog.googlecode.com/svn/data-chatbot/wordnet30 bin/wordnet30/
-svn co https://opensim4opencog.googlecode.com/svn/trunk-chatbot/sources/chatbot sources/chatbot/
+@rem svn co https://opensim4opencog.googlecode.com/svn/trunk-chatbot/sources/chatbot sources/chatbot/
 svn co https://opensim4opencog.googlecode.com/svn/trunk-chatbot/bin-chatbot bin-chatbot/
 svn co https://opensim4opencog.googlecode.com/svn/trunk-logicmoo/prolog/programk bin/prolog/programk
 svn co https://opensim4opencog.googlecode.com/svn/trunk-logicmoo/prolog/startrek bin/prolog/startrek
