4b225b90e3c953b1e180de4b3d995bf5e803d513
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-07-11 07:26:08 +0000

    INSTALLER - uses personalInstallConfig.lisp

---
+++ trunk/bin/personalInstallConfig.lisp
@@ -0,0 +1,9 @@
+
+ (setj INSTALLER_temp "C:%5CUsers%5CAdministrator%5CAppData%5CRoaming%5CCogbot%5CInstallerFiles%5C") 
+ (setj INSTALLER_install "C:%5CProgram%20Files%5CCogbot%5C") 
+ (setj INSTALLER_prolog_location "c:/program%20files/pl") 
+ (setj INSTALLER_botname "ExampleBot%20Resident") 
+ (setj INSTALLER_botpassword "pass123") 
+ (setj INSTALLER_loginuri "https://login.agni.lindenlab.com/cgi-bin/login.cgi") 
+(setj installed_components '( "aiml"  "aiml_personality"  "all"  "core"  "docs"  "examples"  "irc"  "lisp"  "objects"  "opencyc"  "prolog"  "radegast"  "sims"  "wordnet" ))
+
