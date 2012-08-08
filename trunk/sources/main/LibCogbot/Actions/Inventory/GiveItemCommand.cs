190eb8ace5057f534df7c01e91b1ee5975c00f59
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-08-08 23:01:29 +0000

    COGBOT - Back to compiling again

--- trunk/sources/main/LibCogbot/Actions/Inventory/GiveItemCommand.cs
+++ trunk/sources/main/LibCogbot/Actions/Inventory/GiveItemCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Cogbot.World;
+using MushDLR223.Utilities;
 using OpenMetaverse;
 
 using MushDLR223.ScriptEngines;
@@ -18,47 +19,54 @@ namespace Cogbot.Actions.Inventory.Shell
             Description = "Gives items from the current working directory to an avatar or object.";
             Category = CommandCategory.Inventory;
         }
+
         public override CmdResult ExecuteRequest(CmdRequest args)
         {
+            string[] toks = (string[]) args.tokens.Clone();
             if (args.Length < 2)
             {
-                return ShowUsage();// "give <agent/primSpec> <item1> [item2] [item3] [...]";
+                return ShowUsage(); // "give <agent/primSpec> <item1> [item2] [item3] [...]";
             }
-            UUID dest = UUID.Zero;
 
-            List<SimObject> allTargets = new List<SimObject>();
+            string[] objects;
+            if (!args.GetAfter("to", out objects) || objects.Length == 0)
+            {
+                return ShowUsage();
+            }
+            ListAsSet<SimObject> allTargets = GetPrimitiveFromList(objects);
+
             int prepLocatedAt = args.IndexOf("to");
-            int argsUsed;
-            foreach (var VARIABLE in arg)
-            {
-                
-            }
-            List<SimObject> PS = WorldSystem.GetPrimitives(args, out argsUsed);
+            Array.Resize(ref toks, prepLocatedAt);
 
-
-            if (!UUIDTryParse(args, 0, out dest, out argsUsed))
+            int given = 0;
+            foreach (var dest in allTargets)
             {
-                return Failure( "First argument expected agent or object UUID.");
+                given += GiveTo(dest, toks, dest.IsAvatar);
             }
+            return SuccessOrFailure();
+        }
+
+        public int GiveTo(SimObject dest, string[] args, bool moveInsteadOfCopy)
+        {
             InventoryManager Manager = Client.Inventory;
             if (Client.CurrentDirectory == null)
             {
                 Client.CurrentDirectory = Manager.Store.RootFolder;
             }
             string ret = "";
-            for (int i = argsUsed; i < args.Length; ++i)
+            for (int i = 0; i < args.Length; ++i)
             {
                 string inventoryName = args[i];
                 // WARNING: Uses local copy of inventory contents, need to download them first.
                 String found = GiveMatches(Manager, inventoryName,
-                                           FolderContents(Manager, Client.CurrentDirectory.UUID), dest);
+                                           FolderContents(Manager, Client.CurrentDirectory.UUID), dest, moveInsteadOfCopy);
                 if (!string.IsNullOrEmpty(found))
                     ret += "No inventory item named " + inventoryName + " found." + nl;
             }
-            return Success(ret);
+            return args.Length;
         }
 
-        private string GiveMatches(InventoryManager manager, string inventoryName, IEnumerable<InventoryBase> contents, SimObject dest)
+        private string GiveMatches(InventoryManager manager, string inventoryName, IEnumerable<InventoryBase> contents, SimObject dest, bool moveInsteadOfCopy)
         {
             string found = "";
             if (contents != null)
@@ -66,11 +74,11 @@ namespace Cogbot.Actions.Inventory.Shell
                 {
                     if (inventoryName == b.Name || inventoryName == b.UUID.ToString())
                     {
-                        found += GiveAll(manager, b, dest);
+                        found += GiveAll(manager, b, dest, moveInsteadOfCopy);
                     }
                     else if (b is InventoryFolder)
                     {
-                        found += GiveMatches(manager, inventoryName, FolderContents(manager, b.UUID), dest);
+                        found += GiveMatches(manager, inventoryName, FolderContents(manager, b.UUID), dest, moveInsteadOfCopy);
                     }
                 }
             return found;
@@ -81,24 +89,37 @@ namespace Cogbot.Actions.Inventory.Shell
             return manager.FolderContents(fid, Client.Self.AgentID, true, true, InventorySortOrder.ByName, 10000);
         }
 
-        private string GiveAll(InventoryManager manager, InventoryBase b, UUID dest)
+        private string GiveAll(InventoryManager manager, InventoryBase b, SimObject dest, bool moveInsteadOfCopy)
         {
             string ret = "";
-            
+
             if (b is InventoryItem)
             {
                 InventoryItem item = b as InventoryItem;
                 ret += item.Name + nl;
-                manager.GiveItem(item.UUID, item.Name, item.AssetType, dest, true);
+                if (dest.IsAvatar)
+                {
+                    manager.GiveItem(item.UUID, item.Name, item.AssetType, dest.ID, true);
+                }
+                else
+                {
+                    throw new NotImplementedException("giving items to objects");
+                }
             }
             else if (b is InventoryFolder)
             {
+                InventoryFolder item = b as InventoryFolder;
+                if (dest.IsAvatar)
+                {
+                    manager.GiveFolder(item.UUID, item.Name, AssetType.Folder, dest.ID, true);
+                    return item.Name + nl;
+                }
                 InventoryFolder folder = b as InventoryFolder;
-                List<InventoryBase> folderContents = FolderContents(manager,folder.UUID);
+                List<InventoryBase> folderContents = FolderContents(manager, folder.UUID);
                 if (folderContents != null)
                     foreach (InventoryBase list in folderContents)
                     {
-                        ret += GiveAll(manager, list, dest);
+                        ret += GiveAll(manager, list, dest, moveInsteadOfCopy);
                     }
             }
             return ret;
