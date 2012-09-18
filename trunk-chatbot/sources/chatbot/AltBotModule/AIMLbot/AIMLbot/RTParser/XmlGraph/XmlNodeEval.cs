a0a3ec8d740a271442d99f79c6bc595afe720e83
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-09-18 17:10:29 +0000

    CHATBOT - more merging part 4

--- trunk-chatbot/sources/chatbot/AltBotModule/AIMLbot/AIMLbot/RTParser/XmlGraph/XmlNodeEval.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Xml;
-using AltAIMLParser;
-using MushDLR223.ScriptEngines;
-
-namespace RTParser.Utils
-{
-    public delegate IEnumerable<XmlNode> XmlNodeEval(XmlNode src, Request request, OutputDelegate outputDelegate);
-}
