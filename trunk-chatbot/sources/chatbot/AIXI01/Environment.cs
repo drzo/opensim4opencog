99169ea33aa6770d6f75b187a036a52c4b467f73
kino.coursey@gmail.com <kino.coursey@gmail.com@d53da353-1555-0410-a7d6-5d05f50501c2>
2012-10-25 23:48:11 +0000

    Defining how to hookup an AIXI agent to the Mt system

--- trunk-chatbot/sources/chatbot/AIXI01/Environment.cs
+++ trunk-chatbot/sources/chatbot/AIXI01/Environment.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
+using AIMLbot;
+using AltAIMLParser;
+using LogicalParticleFilter1;
+using RTParser;
 
 namespace MCAIXI0
 {
@@ -41,7 +45,131 @@ namespace MCAIXI0
 
 
     }
+    class MtLink : Environment
+    {
+        public string mtName="aixiMt";
+        AltBot envBot = null;
+        Dictionary<string, UInt64> observeDictionary = new Dictionary<string, UInt64>();
+        Dictionary<UInt64, string> actDictionary = new Dictionary<UInt64, string>();
+        Dictionary<string, UInt64> rewardDictionary = new Dictionary<string, UInt64>();
+        UInt64 max_observe = 0;
+        UInt64 max_act = 0;
+        UInt64 max_reward = 0;
 
+        // Defintitions in MTs
+        // aixiActions(SYMBOL,CODE) = action CODE mapps to performAct(SYMBOL) in ouput
+        // aixiObservable(SYMBOL,BIT) = aixiObserve(SYMBOL) sets observe[BIT]=1 
+        // aixiRewards(SYMBOL,VALUE) = seeing aixiFeedback(SYMBOL) adds value to reward
+        //
+        public MtLink(AltBot myBot, string mt)
+        {
+            mtName = mt;
+            envBot = myBot;
+
+            List<Dictionary<string, string>> bingingsList = new List<Dictionary<string, string>>();
+            
+            envBot.servitor.prologEngine.askQuery("aixiObservable(SYMBOL,BIT)", mtName, out bingingsList);
+            foreach (Dictionary<string, string> bindings in bingingsList)
+            {
+                foreach (string key in bindings.Keys)
+                {
+                    string symbol = bindings["SYMBOL"].Trim();
+                    UInt64 bit = UInt64.Parse(bindings["BIT"].Trim());
+                    if (bit > max_observe) max_observe = bit;
+                    observeDictionary[symbol] = bit;
+                }
+            }
+
+            envBot.servitor.prologEngine.askQuery("aixiActions(SYMBOL,CODE)", mtName, out bingingsList);
+            foreach (Dictionary<string, string> bindings in bingingsList)
+            {
+                foreach (string key in bindings.Keys)
+                {
+                    string symbol = bindings["SYMBOL"].Trim();
+                    UInt64 bit = UInt64.Parse(bindings["CODE"].Trim());
+                    if (bit > max_act) max_act = bit;
+                    actDictionary[bit] = symbol;
+                }
+            }
+
+            envBot.servitor.prologEngine.askQuery("aixiRewards(SYMBOL,VALUE)", mtName, out bingingsList);
+            foreach (Dictionary<string, string> bindings in bingingsList)
+            {
+                foreach (string key in bindings.Keys)
+                {
+                    string symbol = bindings["SYMBOL"].Trim();
+                    UInt64 value = UInt64.Parse(bindings["VALUE"].Trim());
+                    if (value > max_reward) max_reward = value;
+                    rewardDictionary[symbol] = value;
+                }
+            }
+
+        }
+
+        public UInt64 decodeReward()
+        {
+            UInt64 reward = 0;
+            List<Dictionary<string, string>> bingingsList = new List<Dictionary<string, string>>();
+            envBot.servitor.prologEngine.askQuery("aixiFeedback(SYMBOL)", mtName, out bingingsList);
+            foreach (Dictionary<string, string> bindings in bingingsList)
+            {
+                foreach (string key in bindings.Keys)
+                {
+                    string symbol = bindings["SYMBOL"].Trim();
+                    if (rewardDictionary.ContainsKey(symbol))
+                    {
+                        reward += (UInt64) rewardDictionary[symbol];
+                    }
+                }
+            }
+            return reward;
+        }
+
+        public UInt64 encodeObservation()
+        {
+            UInt64 observation = 0;
+            List<Dictionary<string, string>> bingingsList = new List<Dictionary<string, string>>();
+            envBot.servitor.prologEngine.askQuery("aixiObserve(SYMBOL)", mtName, out bingingsList);
+            foreach (Dictionary<string, string> bindings in bingingsList)
+            {
+                foreach (string key in bindings.Keys)
+                {
+                    string symbol = bindings["SYMBOL"].Trim();
+                    if (observeDictionary.ContainsKey(symbol))
+                    {
+                        UInt64 bit = observeDictionary[symbol];
+                        UInt64 mask = (UInt64) 1 << (int)bit;
+                        observation = observation | mask;
+                    }
+                }
+            }
+            return observation;
+        }
+
+        public void decodeAction(UInt64 action)
+        {
+            if (actDictionary.ContainsKey((UInt64)action))
+            {
+                string actSymbol = actDictionary[(UInt64)action];
+                string assert = String.Format("performAct({0}).", actSymbol);
+                envBot.servitor.prologEngine.insertKB(assert, "aixiOutputMt");
+            }
+        }
+
+        public override void performAction(UInt64 action)
+        {
+            // Set up the initial observation
+            Random r = new Random();
+            m_last_action = action;
+            decodeAction(action);
+            m_observation = (UInt64)encodeObservation();
+            m_reward = (UInt64)decodeReward();
+
+           // m_observation = (UInt64)(r.NextDouble() < p ? 1 : 0);
+           // m_reward = (UInt64)(action == m_observation ? 1 : 0);
+        }
+
+    }
     class CoinFlip:Environment
     {
         // Constructor: set up the initial environment percept
