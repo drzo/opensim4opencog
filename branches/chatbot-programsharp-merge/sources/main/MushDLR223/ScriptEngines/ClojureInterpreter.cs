20009100bdd442278bedd11f69f117b0f6bbb81a
logicmoo <logicmoo@d53da353-1555-0410-a7d6-5d05f50501c2>
2013-02-13 10:02:38 +0000

    Merged revision(s) 1997 from trunk/lib/MushDLR223: AIMLBOT - don't say "RANDOM TOPIC" ........

---
+++ branches/chatbot-programsharp-merge/sources/main/MushDLR223/ScriptEngines/ClojureInterpreter.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MushDLR223.Utilities;
+
+namespace MushDLR223.ScriptEngines
+{
+    public class ClojureInterpreter : DotLispInterpreterBase, ScriptInterpreter
+    {
+
+        public override void Dispose()
+        {
+            dotLispInterpreter.Dispose();
+        }
+
+        public override void InternType(Type t)
+        {
+            EnsureInit();
+            dotLispInterpreter.InternType(t);
+        }
+
+        public override object GetSymbol(string eventName)
+        {
+           // object r = Eval(base.ReadFromString(eventName));
+            //eventName = eventName.ToLower();
+            DotLisp.Symbol o = dotLispInterpreter.intern(eventName);//.Read("DefinedFunction", new System.IO.StringReader(eventName));           
+            return o;
+        }
+
+        public override bool IsSubscriberOf(string eventName)
+        {
+            DotLisp.Symbol o = dotLispInterpreter.intern(eventName);//.Read("DefinedFunction", new System.IO.StringReader(eventName));           
+            return o.isDefined();
+        }
+
+        public ClojureInterpreter(object self)
+            : base(self)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public override bool LoadFile(string filename, OutputDelegate WriteLine)
+        {
+            if (!filename.EndsWith(".cloj"))
+            {
+                filename = filename + ".cloj";
+            }
+            System.IO.FileInfo fi = new System.IO.FileInfo(filename);
+            if (fi.Exists)
+            {
+                dotLispInterpreter.LoadFile(filename);
+                return true;
+            }      
+            return false;
+        }
+
+        public override bool LoadsFileType(string filename)
+        {
+            return filename.EndsWith("cloj") || base.LoadsFileType(filename);
+        }
+
+// method: LoadFile
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context_name"></param>
+        /// <param name="stringCodeReader"></param>
+        /// <returns></returns>
+        public override object Read(string context_name, System.IO.TextReader stringCodeReader, OutputDelegate WriteLine)
+        {
+            return dotLispInterpreter.Read(context_name, stringCodeReader);
+        } // method: Read
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="codeTree"></param>
+        /// <returns></returns>
+        public override bool Eof(object codeTree)
+        {
+           return dotLispInterpreter.Eof(codeTree);
+        } // method: Eof
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="varname"></param>
+        /// <param name="textForm"></param>
+        public override void Intern(string varname, object value)
+        {
+            if (value!=null)
+            {
+                InternType(value.GetType());
+            }
+           dotLispInterpreter.Intern(varname, value);
+        } // method: Intern
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public override object Eval(object code)
+        {
+            return dotLispInterpreter.Eval(code);
+        } // method: Eval
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public override string Str(object code)
+        {
+            return dotLispInterpreter.Str(code);
+        } // method: Str
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override DotLispInterpreterBase MakeInterp(object self)
+        {
+            var v = new ClojureInterpreter(self);
+            v.Intern("*SELF*", self);
+            return v;
+        } // method: newInterpreter
+
+    }
+}
