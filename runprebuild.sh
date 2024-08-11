3b83801c4148a0438259e88997b0a53acb82f123
drzo <d@rzo.io>
2024-08-11 20:10:02 +0000

    2

--- runprebuild.sh
+++ runprebuild.sh
@@ -8,43 +8,23 @@ echo START: copy assemblies
 
 # Check if directories exist before copying
 if [ -d "sources/external/Radegast/assemblies" ]; then
-    cp sources/external/Radegast/assemblies/*.dll bin/
+	cp sources/external/Radegast/assemblies/*.dll bin/
 else
-    echo "Directory sources/external/Radegast/assemblies does not exist"
+	echo "Directory sources/external/Radegast/assemblies does not exist"
 fi
 
 if [ -d "sources/external/Radegast" ]; then
-    cp sources/external/Radegast/*.dll bin/
-    cp sources/external/Radegast/*.pdb bin/
-    cp sources/external/Radegast/*.config bin/
-    cp sources/external/Radegast/*.so bin/
-    cp sources/external/Radegast/*.dylib bin/
+	cp sources/external/Radegast/*.dll bin/
+	cp sources/external/Radegast/*.pdb bin/
+	cp sources/external/Radegast/*.config bin/
+	rm -f sources/external/Radegast/libop*
+	cp sources/external/Radegast/*.so bin/
+	cp sources/external/Radegast/*.dylib bin/
 else
-    echo "Directory sources/external/Radegast does not exist"
+	echo "Directory sources/external/Radegast does not exist"
 fi
 
-# Check if svn is installed
-if command -v svn >/dev/null 2>&1; then
-    svn revert bin/LAIR*.dll
-    svn revert bin/LAIR.MachineLearning.dll
-    svn revert bin/LAIR.Collect*.dll
-    svn revert bin/*.dll bin/*.exe bin/*.so
-else
-    echo "svn is not installed"
-fi
-
-if [ -d "sources/external/LAIR.ResourceAPIs/PennBank/TreeBankGrapher/bin/Release" ]; then
-    cp sources/external/LAIR.ResourceAPIs/PennBank/TreeBankGrapher/bin/Release/*.dll bin/
-else
-    echo "Directory sources/external/LAIR.ResourceAPIs/PennBank/TreeBankGrapher/bin/Release does not exist"
-fi
-
-#cp sources/external/Radegast/*.ico /tmp
-#cp -a sources/external/Radegast/Resources/ /tmp/
-
-#rm -f sources/external/LookingGlass-svn/bin/Prebuild.exe
-#rm -f sources/external/LookingGlass-svn/bin/OpenMeta*
-#rm -f sources/external/LookingGlass-svn/bin/HttpServer.dll
+svn revert bin/LAIR*.dll
 
 echo DONE: copy assemblies
 
@@ -52,12 +32,41 @@ echo START: Generating NANT build files
 
 # Check if mono is installed
 if command -v mono >/dev/null 2>&1; then
-    # Replace 'some_command_here' with the actual command you need to run
-    mono actual_command_here
+	# Replace 'path/to/your/actual_assembly.exe' with the actual command you need to run
+	mono bin/your_actual_assembly.exe
 else
-    echo "mono is not installed"
+	echo "mono is not installed"
 fi
 
 # Add more commands here as needed
 
 echo DONE: Generating NANT build files
+
+# Ensure directories exist before copying NullBuild.txt
+if [ -d "./sources/external/LAIR.ResourceAPIs/Wikipedia/MirrorTables" ]; then
+	cp NullBuild.txt ./sources/external/LAIR.ResourceAPIs/Wikipedia/MirrorTables/MirrorTables.exe.build
+else
+	echo "Directory ./sources/external/LAIR.ResourceAPIs/Wikipedia/MirrorTables does not exist"
+fi
+
+if [ -d "./bin" ]; then
+	cp NullBuild.txt ./bin/CogbotLib.dll.build
+else
+	echo "Directory ./bin does not exist"
+fi
+
+if [ -d "./sources/external/LAIR.ResourceAPIs/Cogbot.LAIR" ]; then
+	cp NullBuild.txt ./sources/external/LAIR.ResourceAPIs/Cogbot.LAIR/Cogbot.LAIR.exe.build
+else
+	echo "Directory ./sources/external/LAIR.ResourceAPIs/Cogbot.LAIR does not exist"
+fi
+
+echo DONE: Generating NANT build files
+
+# Remove specific files
+rm -rf bin/RadSpeechWin*.*
+
+echo To just build CSProloge.exe
+echo "./runprebuild.sh ;  find -iname \"*.build\" -not -name Cogbot.build -not -name CSProlog.*.build -exec cp NullBuild.txt '{}' \; ; nant "
+
+echo otherwise type: nant
